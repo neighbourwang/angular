3d0ead8761f89aa1bc778a058e9151f1f123ed8a
alan_cao <zongying.cao@hpe.com>
2016-10-09 11:41:41 +0800

    添加验证

--- backend/src/module/ent-mng/ent-est-mng/component/ent-est-cre-step-01.component.ts
+++ backend/src/module/ent-mng/ent-est-mng/component/ent-est-cre-step-01.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { LayoutService } from '../../../../architecture';
+import { LayoutService, ValidationService, NoticeComponent } from '../../../../architecture';
 import { EntEstBasicInfo } from '../model/ent-est-basic-info'
 import { EntEstCreService } from '../service/ent-est-cre.service'
 import { CurrencyType } from "../model/currency";
@@ -9,33 +9,81 @@ import { CurrencyType } from "../model/currency";
 	selector:'ent-est-cre-step-01'
 	,templateUrl:'../template/ent-est-cre-step-01.component.html'
 	,styleUrls:[]
-	,providers:[EntEstCreService]
+	,providers:[
+		EntEstCreService
+		,ValidationService
+		]
 })
 export class EntEstCreStep01Component implements OnInit{
+	@ViewChild('notice')
+    notice: NoticeComponent;
+
 	private entEstBasicInfo:EntEstBasicInfo;
 	private currencyTypes : CurrencyType[] = [];
 	constructor(
 		private router: Router,
-		private service: EntEstCreService
+		private service: EntEstCreService,
+		private validation : ValidationService
 		){
 
 	}
 	ngOnInit(){
 		this.entEstBasicInfo = this.service.getEntEst().BasicInfo;
-		console.log('this.entEstBasicInfo load:', this.entEstBasicInfo);
-		this.service.loadCurrencyTypes(this.currencyTypes, this.showNotice);
+		this.service.loadCurrencyTypes(this.currencyTypes, null);
 	}
 
 	next(){
-		console.log('this.entEstBasicInfo save:', this.entEstBasicInfo);
+		if(!this.validate())
+			return;
+
       	this.router.navigateByUrl("ent-mng/ent-est-mng/ent-est-cre-step-02");
+	}
+
+	showMsg(msg: string)
+	{
+		this.notice.open("系统提示", msg);
+	}
+
+	validate():boolean{
+
+		let funcMap :any = {
+			"*":{
+				"func":this.validation.isBlank
+				,"msg":"不能为空"
+			}
+			,"email":{ 
+				"func": this.validation.isEmail
+				,"msg": "邮箱地址无效"
+			}
+		};
+
+		let func = function(name:string, val:any, op:string){
+			if(funcMap[op].func(val))
+			{
+				return name + funcMap[op].msg;
+			}
+			else
+				return "";
+		}
+
+		let notValids = [
+		{
+			"name":"名称"
+			,'value':this.entEstBasicInfo.name
+			,"op":"*"
+		}].filter(n=>func(n.name, n.value, n.op) != "")
+
+		if(notValids.length > 0)
+		{
+			let notValid = notValids.shift();
+			this.showMsg(func(notValid.name, notValid.value, notValid.op));
+			return false;
+		}
+
+		return true;
 	}
 
 	cancel(){
 		this.router.navigateByUrl('ent-mng/ent-est-mng/ent-est-mng');
-	}
-
-	showNotice(title, desc){
-
 	}
 }
