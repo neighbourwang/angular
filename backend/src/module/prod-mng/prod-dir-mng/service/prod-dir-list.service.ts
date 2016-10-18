170ebd87db34a26a1e8b232d551096fee8066397
neighbourwang <yao-yao.wang@hpe.com>
2016-10-18 15:09:49 +0800

    pro -dir bug none

---
+++ backend/src/module/prod-mng/prod-dir-mng/service/prod-dir-list.service.ts
@@ -0,0 +1,25 @@
+/**
+ * Created by wangyao on 2016/10/18.
+ */
+// import { Injectable } from '@angular/core';
+// import { Http, Response } from '@angular/http';
+// import { RestApiCfg, RestApi, RestApiModel } from '../../../../architecture';
+//
+//
+// import 'rxjs/add/operator/toPromise';
+//
+// @Injectable()
+// export class ProdDirListService {
+//     constructor(
+//         private http: Http,
+//         private restApiCfg: RestApiCfg,
+//         private restApi: RestApi
+//     ) { }
+//
+//     // 取得所有产品目录
+//     getPlatforms(page: number, size: number) {
+//         let api = this.restApiCfg.getRestApi("pf.conn.mng.platforms.get");
+//
+//         return this.restApi.request(api.method, api.url, [{ key: "page", value: page }, { key: "size", value: size }], undefined);
+//     }
+// }
