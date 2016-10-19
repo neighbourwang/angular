88c15c200b015175894af9f9a0714d435073f3a0
YXYFuture <1247676987@qq.com>
2016-10-19 16:56:48 +0800

    查看企业

---
+++ backend/src/module/ent-mng/ent-est-mng/component/ent-est-check.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, ViewChild, } from '@angular/core';
+import { Router } from '@angular/router';
+import { LayoutService, NoticeComponent, PopupComponent } from '../../../../architecture';
+import { EntEstItem, EntEst} from '../model';
+
+import { EntEstCreService, Paging } from '../service/ent-est-cre.service';
+
+@Component({
+  // moduleId: module.id,
+  selector: 'ent-est-mng',
+  templateUrl: '../template/ent-est-check.component.html',
+  styleUrls: ['../style/ent-est-mng.component.css'],
+  providers: [EntEstCreService]
+}) 
+export class EntEstCheckComponent implements OnInit {
+  @ViewChild("notice")
+  notice: NoticeComponent;
+
+  @ViewChild("editEnt")
+  editEnt: PopupComponent;
+
+  @ViewChild("editQuota")
+  editQuota: PopupComponent;
+
+  @ViewChild("setupCert")
+  setupCert: PopupComponent;
+
+
+
+  constructor(
+    private layoutService: LayoutService,
+    private router: Router,
+    private service: EntEstCreService
+  ) {}
+
+  ngOnInit() {
+   
+  }
+
+  
+
+}
