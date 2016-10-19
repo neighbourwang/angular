e9fe8fd87adfb7472bd8d8081c163e96cbfb47f7
zhangjunjie <865524326@qq.com>
2016-10-19 11:00:11 +0800

    list页面 删除 启用 禁用

--- backend/src/module/pf-mng2/cl-mng/service/cl-mgn-list.service.ts
+++ backend/src/module/pf-mng2/cl-mng/service/cl-mgn-list.service.ts
@@ -23,4 +23,25 @@ export class ClMngListService {
 
         return this.restApi.request(api.method, api.url, [{ key: "page", value: page }, { key: "size", value: size }], undefined);
     }
+
+    // 删除特定平台信息
+    deletePlatform(pid: String) {
+        let api = this.restApiCfg.getRestApi("pf.cre.paltform.delete");
+
+        return this.restApi.request(api.method, api.url, [{ key: "pf-id", value: pid }], undefined, undefined);
+    }
+
+    // 启用特定平台
+    activePlatform(pid: String) {
+        let api = this.restApiCfg.getRestApi("pf.cre.paltform.active.get");
+
+        return this.restApi.request(api.method, api.url, [{ key: "pf-id", value: pid }], undefined);
+    }
+
+    //禁用平台
+    disable(pid:String){
+        let api = this.restApiCfg.getRestApi("pf.cre.platform.disable.put");
+
+        return this.restApi.request(api.method , api.url , [{key : "_id",value : pid}],undefined)
+    }
 }
