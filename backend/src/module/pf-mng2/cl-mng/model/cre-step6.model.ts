4bffa5c7756331e3891b2e755e7eb8b8a4c0a04c
zhangjunjie <865524326@qq.com>
2016-10-20 14:48:21 +0800

    失败

--- backend/src/module/pf-mng2/cl-mng/model/cre-step6.model.ts
+++ backend/src/module/pf-mng2/cl-mng/model/cre-step6.model.ts
@@ -1,9 +1,6 @@
 export class CreStep6Model {
 
-    // "description": "string",
-    //   "format": "string",
-    //   "id": "string",
-    //   "name": "string"
+    
     name : String ;// 名称
     format : String ;//格式
     description : String;//说明
