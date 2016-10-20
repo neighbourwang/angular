62086889f0431e9e243c34324e306fedc1c9c4a3
neighbourwang <yao-yao.wang@hpe.com>
2016-10-20 09:58:23 +0800

    add prod edit dialog

--- backend/src/module/prod-mng/prod-mng/model/product.model.ts
+++ backend/src/module/prod-mng/prod-mng/model/product.model.ts
@@ -2,5 +2,14 @@
  * Created by wangyao on 2016/10/19.
  */
 export class Product{
-    
+    createrName: string;
+    creatorId: string;
+    description: string;
+    productNum: number;
+    serviceId: string;
+    serviceName: string;
+    specification:string;
+    status: string;
+    isSelected : boolean;
+
 }
