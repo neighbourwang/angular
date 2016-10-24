68bd38415a0cc366753138436e7e799619136b64
neighbourwang <yao-yao.wang@hpe.com>
2016-10-24 11:18:00 +0800

    monday pull

--- backend/src/architecture/components/countBar/component/count-bar.component.ts
+++ backend/src/architecture/components/countBar/component/count-bar.component.ts
@@ -1,28 +1,71 @@
 /**
  * Created by wangyao on 2016/10/21.
  */
-import { Component, Input,Output } from '@angular/core';
+import {Component, Input, Output,EventEmitter,OnInit} from '@angular/core';
 //配置数据
 import {Config} from '../config/Config';
 @Component({
     selector: 'count-bar',
     templateUrl: '../template/count-bar.component.html',
     // inputs: ["title", "msg", "ot", "ct"]
+    styles: [`
+        span{
+                border：1px solid #C1CACA;
+            }
+        .countBar{
+            span{
+                border：1px solid #C1CACA;
+            }    
+            input{
+                width:40px;
+                border-top:1px solid #C1CACA;
+                border-bottom:1px solid #C1CACA;
+                text-align:center;
+                line-height:34px;
+            }
+        }
+    `]
 })
-export class CountBarComponent{
-    constructor(){
+export class CountBarComponent implements OnInit{
+    constructor() {
 
     }
-    
+    value:number;
+
+
     @Input()
     config:Config;
 
-    
-    add(){
-        console.log(this.config);
-        this.config.value+=this.config.step;
+
+    // @Output()
+    // output=new EventEmitter();
+
+    ngOnInit (){
+        console.log('init');
+
+        this.value=this.config.default;
+    }
+
+    add (){
+        this.value += this.config.step;
+            // this.output.emit(this.value);
     }
     subtract(){
-        this.config.value-=this.config.step;
+        this.value -= this.config.step;
+    }
+    // add() {
+    //     // console.log(this.config);
+    //     this.value += this.config.step;
+    //     this.output.emit(this.value);
+    // }
+    //
+    // subtract() {
+    //     this.value -= this.config.step;
+    //     this.output.emit(this.value);
+    // }
+
+    outputValue(){
+        // this.config.name='countBar'+this.config.name;
+
     }
 }
