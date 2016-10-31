7bbe3144f9a3e696616c8788ef10ed837f8999d5
YXYFuture <1247676987@qq.com>
2016-10-31 15:25:25 +0800

    目录搭建和订单详情html

---
+++ backend/src/module/op-center/order-mng/component/order-mng-detail.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, ViewChild, } from '@angular/core';
+import { Router } from '@angular/router';
+import { RestApi, RestApiCfg, LayoutService, NoticeComponent, PopupComponent, ConfirmComponent, SystemDictionaryService, SystemDictionary } from '../../../../architecture';
+
+
+@Component({
+  // moduleId: module.id,
+  selector: 'ent-est-mng',
+  templateUrl: '../template/order-mng-detail.component.html',
+  styleUrls: ['../style/order-mng-detail.less'],
+  providers: []
+}) 
+export class OrderMngDetailComponent implements OnInit {
+  @ViewChild("notice")
+  notice: NoticeComponent;
+
+  @ViewChild("editEnt")
+  editEnt: PopupComponent;
+
+  @ViewChild("editQuota")
+  editQuota: PopupComponent;
+
+  @ViewChild("setupCert")
+  setupCert: PopupComponent;
+
+  @ViewChild("confirm")
+  confirm: ConfirmComponent;
+
+  private totalPages: number = 0;
+  private currentPage: number = 0;
+  private selectAllField: boolean = false;
+  private criteria: string = "";
+
+  constructor(
+    private layoutService: LayoutService,
+    private router: Router,
+    private sysDicService: SystemDictionaryService,
+    private restApiCfg:RestApiCfg,
+    private restApi:RestApi
+  ) {
+    
+  }
+}
