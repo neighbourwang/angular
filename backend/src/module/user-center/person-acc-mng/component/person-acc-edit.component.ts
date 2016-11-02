55222991ba5d2aba29123b136736012c44bbfb1f
neighbourwang <yao-yao.wang@hpe.com>
2016-11-02 16:41:32 +0800

    user-center person-acc-mng html done

---
+++ backend/src/module/user-center/person-acc-mng/component/person-acc-edit.component.ts
@@ -0,0 +1,37 @@
+import { Component, ViewChild, OnInit, SimpleChange } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { LayoutService, ValidationService, NoticeComponent, CountBarComponent } from '../../../../architecture';
+
+@Component({
+    selector: 'person-acc-edit',
+    templateUrl: '../template/person-acc-edit.component.html',
+    styleUrls: [],
+    providers: []
+})
+
+export class PersonAccEditComponent implements OnInit {
+    constructor(
+        private router: Router,
+        // private ProdDirDetailService: ProdDirDetailService,
+        // private ProdListService : ProdListService,
+        // private ProdDirListService: ProdDirListService,
+        // private PostProduct:PostProduct
+    ) { }
+
+    // @ViewChild('notice')
+    // notice: NoticeComponent;
+
+    // enterpriseList = new Array();
+    // prodDirList = new Array();
+    // prodDir = new ProductDir();
+    // prodDirId:string;
+    // product=new Product();   
+    ngOnInit() {}
+    cancel(){
+        this.router.navigate(['user-center/person-acc-mng/person-acc-mng']);
+    }
+    onSubmit(){
+        
+    }
+}
