ef07128f8af11a984d495409e965eb38a5ce6540
Huidan Fan <huidan.fan@hpe.com>
2016-11-07 09:36:45 +0800

    angular

--- frontend/src/app.routing.ts
+++ frontend/src/app.routing.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        redirectTo: 'cloud-host-service/cloud-host-list',
+        redirectTo: 'image-mng/image-mng-fhd',
         pathMatch: 'full'
     }
 ];
