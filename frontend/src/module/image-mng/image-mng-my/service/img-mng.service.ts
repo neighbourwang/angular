d7217f1291f9ce8d1bba69e6f541c4a8bebe32dd
mayan <304426925@qq.com>
2016-11-08 11:10:22 +0800

    my-service修改

--- frontend/src/module/image-mng/image-mng-my/service/img-mng.service.ts
+++ frontend/src/module/image-mng/image-mng-my/service/img-mng.service.ts
@@ -5,6 +5,9 @@ import { RestApiCfg, RestApi } from "../../../../architecture";
  
 import "rxjs/add/operator/toPromise";
 
+import { imageList } from "../model/images-mock.model";
+import { Image } from "../model/image.model";
+
 @Injectable()
 export class ImgMngService_my {
     constructor(
@@ -18,8 +21,24 @@ export class ImgMngService_my {
         this.restApiCfg.loadCfgData();
     }
 
-    
+    getImages():Promise<any>{
+        const pathParams=[
+            {
+                key:"page",
+                value:1
+            },
+            {
+                key:"size",
+                value:1
+            }
+        ]
+        const api = this.restApiCfg.getRestApi("image.mng.list");
+        return this.restApi.request(api.method, api.url, pathParams, null, null);
+    }
 
+    getImagesByMock():Promise<any>{
+        return new Promise(resovle => setTimeout(resovle, 200)).then(() => imageList);
+    }
     
 }
 
