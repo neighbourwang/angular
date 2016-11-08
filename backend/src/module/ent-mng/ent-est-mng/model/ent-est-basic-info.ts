a6770aadba296a27920194323504dc8577d9d787
cao,zongying <zongying.cao@hpe.com>
2016-11-08 15:19:00 +0800

    完善创建企业“

--- backend/src/module/ent-mng/ent-est-mng/model/ent-est-basic-info.ts
+++ backend/src/module/ent-mng/ent-est-mng/model/ent-est-basic-info.ts
@@ -13,6 +13,8 @@ export class EntEstBasicInfo{
 	password: string = null;//密码
 	description: string = null;//描述
 	logo: string = null;//logo
+	isSSL: boolean = null;// 是否进行ssl加密
+	accountAttribute:string = null;//登录账户名属性
 
 	reset(){
 		this.code = null;
@@ -27,7 +29,8 @@ export class EntEstBasicInfo{
 		this.password = null;
 		this.description = null;
 		this.logo = null;
-
+		this.isSSL = null;
+		this.accountAttribute = null;
 	}
 }
 
