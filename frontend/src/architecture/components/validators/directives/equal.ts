b14f4f0e03a45003d992dc1149e611f6fba82f64
zhonghui <zhonghui@b5m.com>
2016-11-09 13:48:20 +0800

    初始化验证表单组件

---
+++ frontend/src/architecture/components/validators/directives/equal.ts
@@ -0,0 +1,28 @@
+import { Directive, Input, forwardRef, OnInit } from '@angular/core';
+import { NG_VALIDATORS, Validator, ValidatorFn, AbstractControl } from '@angular/forms';
+
+import { CustomValidators } from '../';
+
+const EQUAL_VALIDATOR: any = {
+    provide: NG_VALIDATORS,
+    useExisting: forwardRef(() => EqualValidator),
+    multi: true
+};
+
+@Directive({
+    selector: '[equal][formControlName],[equal][formControl],[equal][ngModel]',
+    providers: [EQUAL_VALIDATOR]
+})
+export class EqualValidator implements Validator, OnInit {
+    @Input() equal: any;
+
+    private validator: ValidatorFn;
+
+    ngOnInit() {
+        this.validator = CustomValidators.equal(this.equal);
+    }
+
+    validate(c: AbstractControl): {[key: string]: any} {
+        return this.validator(c);
+    }
+}
