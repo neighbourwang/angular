7e79f52036aad5962640ff81494c652e0d5688ad
Chuyu Guo <chuyu.guo@hpe.com>
2016-11-09 11:07:03 +0800

    icon and img-mng-gcy

---
+++ frontend/src/module/image-mng/image-mng-gcy/model/img-mng.img.ts
@@ -0,0 +1,13 @@
+export class ImageAttr_gcy{
+    imageName : string;
+    imageType : string;
+    osName : string;
+    osDigits: string;
+    createdDate: string;
+    status: string;
+    progress: string;
+    location: string;
+    description: string;
+    constructor() {
+    }
+}
--- frontend/src/module/image-mng/image-mng-gcy/service/img-mng.service.ts
+++ frontend/src/module/image-mng/image-mng-gcy/service/img-mng.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from "@angular/core";
 import { Http, Response } from "@angular/http";
 import { RestApiCfg, RestApi } from "../../../../architecture";
 
+import {ImageAttr_gcy} from "../model/img-mng.img"
+import {ImageData_gcy} from "../model/img-mng.mock-img";
  
 import "rxjs/add/operator/toPromise";
 
@@ -18,7 +20,24 @@ export class ImgMngService_gcy {
         this.restApiCfg.loadCfgData();
     }
 
-    
+    getImages(pageIndex: number, pageSize: number): Promise<any> {
+        const pathParams = [
+            {
+                key: "page",
+                value: pageIndex
+            },
+            {
+                key: "size",
+                value: pageSize
+            }
+        ];
+
+
+        //const api = this.restApiCfg.getRestApi("image.mng.list");
+        //return this.restApi.request(api.method, api.url, pathParams, null, null);
+
+        return new Promise(resovle => setTimeout(resovle, 200)).then(() => {return ImageData_gcy});
+    }
 
     
 }
