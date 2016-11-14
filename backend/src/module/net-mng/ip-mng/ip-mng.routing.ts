ac5bfb44f0cb4ba5773bb555802a6df5d9f2fffd
LilyWei <xiao-li.wei@hpe.com>
2016-11-14 15:00:19 +0800

    for ip mng

---
+++ backend/src/module/net-mng/ip-mng/ip-mng.routing.ts
@@ -0,0 +1,15 @@
+import { ModuleWithProviders } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { IpMngListComponent } from './component/ip-mng-list.component';
+
+export const IpMngRouting:ModuleWithProviders = RouterModule.forChild([
+    {
+        path: 'pf-mng2/cl-mng/cl-mng',
+        component: IpMngListComponent
+    },
+    {
+        path : 'pf-mng2/cl-mng/cre-step6',
+        component : IpMngListComponent
+    }
+]);
