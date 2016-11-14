e2b3f84497d1755f79dfeaae33c23aecfe0139aa
neighbourwang <yao-yao.wang@hpe.com>
2016-11-14 18:52:34 +0800

    person acc mng done v1

--- frontend/src/module/user-center/person-acc-mng/component/person-acc-mng.component.ts
+++ frontend/src/module/user-center/person-acc-mng/component/person-acc-mng.component.ts
@@ -1,7 +1,7 @@
-import { Component, ViewChild, OnInit} from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { LayoutService, ValidationService,PopupComponent} from '../../../../architecture';
+import { LayoutService, ValidationService, PopupComponent,NoticeComponent } from '../../../../architecture';
 
 //service
 import { GetPersonAccService } from '../service/person-acc-get.service';
@@ -9,7 +9,7 @@ import { PutPersonAccService } from '../service/person-acc-put.service';
 import { EditPersonAccPwdService } from '../service/person-acc-pwd.service';
 
 //model
-import { PersonAcc, PersonAccPwd} from '../model/person-acc.model';
+import { PersonAcc, PersonAccPwd } from '../model/person-acc.model';
 
 @Component({
     selector: 'person-acc-mng',
@@ -23,12 +23,12 @@ export class PersonAccMngComponent implements OnInit {
         private router: Router,
         private getPersonAcc: GetPersonAccService,
         private putPersonAcc: PutPersonAccService,
-        private putPersonAccPwd:EditPersonAccPwdService
+        private putPersonAccPwd: EditPersonAccPwdService
     ) { }
-     @ViewChild('editPassWord')
+    @ViewChild('editPassWord')
     editPassWord: PopupComponent;
-    // @ViewChild('notice')
-    // notice: NoticeComponent;
+    @ViewChild('notice')
+    notice: NoticeComponent;
     personAcc: PersonAcc = new PersonAcc();
     temPersonAcc: PersonAcc = new PersonAcc();
 
@@ -53,94 +53,158 @@ export class PersonAccMngComponent implements OnInit {
             });
     }
     //编辑账号
-    onEdit(){
+    onEdit() {
         this.router.navigate(['user-center/person-acc-mng/person-acc-edit'])
-    }    
+    }
 
     //编辑姓名
-    name:boolean=true;
-    nameEdit:boolean=false;
-    editName(){
-        this.name=false;
-        this.nameEdit=true;
+    name: boolean = true;
+    nameEdit: boolean = false;
+    userNameValid: boolean = true;
+    editName() {
+        this.name = false;
+        this.nameEdit = true;
+        this.cancelPhone();
+        this.cancelDesc();
     }
-    saveName(){
-        this.name=true;
-        this.nameEdit=false;
+    saveName() {
+        if (this.personAcc.userName) {
+            console.log(this.personAcc);
+            this.saveEditPerAcc().then(() => {
+                this.name = true;
+                this.nameEdit = false;
+            });
+        } else {
+            this.userNameValid = false;
+        }
     }
-    cancelName(){
-        this.name=true;
-        this.nameEdit=false;
+    cancelName() {
+        this.personAcc.userName = this.temPersonAcc.userName;
+        this.name = true;
+        this.nameEdit = false;
     }
 
     //编辑电话
-    phone:boolean=true;
-    phoneEdit:boolean=false;
-    editPhone(){
-        this.phone=false;
-        this.phoneEdit=true;
+    phone: boolean = true;
+    phoneEdit: boolean = false;
+    editPhone() {
+        this.phone = false;
+        this.phoneEdit = true;
+        this.cancelName();
+        this.cancelDesc();
     }
-    savePhone(){
-        this.phone=true;
-        this.phoneEdit=false;
-    }
-    cancelPhone(){
-        this.phone=true;
-        this.phoneEdit=false;
-    }
+    savePhone() {
+        if (this.personAcc.phone) {
+            this.saveEditPerAcc().then(() => {
+                this.phone = true;
+                this.phoneEdit = false;
+            });
+        } else {
 
-    //编辑描述
-    desc:boolean=true;
-    descEdit:boolean=false;
-    editDesc(){
-        this.desc=false;
-        this.descEdit=true;
-    }
-    saveDesc(){
-        this.desc=true;
-        this.descEdit=false;
-    }
-    cancelDesc(){
-        this.desc=true;
-        this.descEdit=false;
-    }
-    //编辑密码
-    accPwd:PersonAccPwd = new PersonAccPwd();
-    samePwd:boolean=false;
-    comparePwd(){
-        if(this.accPwd.newPassword!==this.accPwd.confirmPwd){
-            this.samePwd=true;
         }
     }
+    cancelPhone() {
+        this.personAcc.phone = this.temPersonAcc.phone;
+        this.phone = true;
+        this.phoneEdit = false;
+    }
+    //编辑描述
+    desc: boolean = true;
+    descEdit: boolean = false;
+    editDesc() {
+        this.desc = false;
+        this.descEdit = true;
+        this.cancelName();
+        this.cancelPhone();
+    }
+    saveDesc() {
+        this.saveEditPerAcc().then(() => {
+            this.desc = true;
+            this.descEdit = false;
+        });
+    }
+    cancelDesc() {
+        this.personAcc.description = this.temPersonAcc.description;
+        this.desc = true;
+        this.descEdit = false;
+    }
+    //编辑密码
+    accPwd: PersonAccPwd = new PersonAccPwd();
+    passwordValid: boolean = true;
+    newPasswordValid: boolean = true;
+    sameNewPassword:boolean=false;
+    samePassword: boolean = true;
+        
+    // comparePwd() {
+    //     if (this.accPwd.newPassword == this.accPwd.confirmPwd) {
+    //         this.samePwd = true;
+    //     }else{
+
+    //     }
+    // }
     editPwd() {
+        this.accPwd= new PersonAccPwd();
+        this.samePassword=true;
+        this.passwordValid= true;
+        this.newPasswordValid= true;
+        this.sameNewPassword=false;
+        this.accPwd.password='';
+        this.accPwd.newPassword='';
+        this.accPwd.confirmPwd='';
         this.editPassWord.open('修改密码')
     }
     otEditPwd() {
-        this.accPwd.id=this.personAcc.id;
         console.log(this.accPwd);
-        this.putPersonAccPwd.putPersonAccPwd(this.accPwd).then(
+        if(this.accPwd.password&&this.accPwd.password.trim()!=''){            
+            this.passwordValid=true;
+        }else{
+            this.passwordValid=false;
+            return;
+        }
+        if(this.accPwd.newPassword&&this.accPwd.newPassword.trim()!=''){
+            this.newPasswordValid=true;            
+        }else{
+            this.newPasswordValid=false; 
+            return;
+        }
+        if(this.accPwd.password==this.accPwd.newPassword){
+            this.sameNewPassword=true; 
+            return;
+        }     
+
+        if(this.accPwd.newPassword == this.accPwd.confirmPwd){
+            this.accPwd.id = this.personAcc.id;
+            console.log(this.accPwd);
+            this.putPersonAccPwd.putPersonAccPwd(this.accPwd).then(
             response => {
                 if (response && 100 == response.resultCode) {
                     console.log(response);
-                } else {
-
-                }
+                    this.editPassWord.close();
+                } 
             }).catch((err) => {
-                console.error(err);
+                this.editPassWord.close();
+                this.notice.open('操作错误','you have input wrong password')
             });
+        }else{
+            this.samePassword=false;
+        }        
     }
     ccf() {
 
     }
-    saveEditPerAcc(){
-        this.putPersonAcc.putPersonAcc(this.personAcc.id,this.personAcc).then(response=>{
+    nof(){
+        
+    }
+    saveEditPerAcc() {
+        return this.putPersonAcc.putPersonAcc(this.personAcc.id, this.personAcc).then(response => {
             console.log(response);
-        }).catch(err=>{
+            this.getCurrentAccount();
+        }).catch(err => {
             console.error(err);
         })
     }
-    clear(obj){
-        console.log(obj);
-        obj.value="";
+    clear(val) {
+        console.log(val);
+        this.personAcc[val] = '';
     }
 }
