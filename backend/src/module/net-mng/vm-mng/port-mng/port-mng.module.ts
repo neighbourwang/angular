31f06755fc4b829b0f65d229cbb98568e69a2a25
Moses <jian.zhou6@hpe.com>
2016-11-18 00:16:25 +0800

    add vm-mng/port-mng

---
+++ backend/src/module/net-mng/vm-mng/port-mng/port-mng.module.ts
@@ -0,0 +1,40 @@
+﻿/**
+ * Created by wangyao on 2016/10/18.
+ */
+import { NgModule } from '@angular/core';
+
+// Common Components
+import { CommonComponentModule } from '../../../../architecture';
+
+//Components
+import { PortMngComponent } from './component/port-mng.component';
+import { PortMngSetComponent } from './component/port-mng-set.component';
+
+// Routing
+import { VMPortMngRouting } from './port-mng.routing';
+
+
+//Service
+import { PortMngService} from "./service/port-mng.service";
+import { PortMngSetService } from "./service/port-mng-set.service";
+
+@NgModule({
+    imports: [
+        CommonComponentModule,
+        VMPortMngRouting
+    ],
+    declarations: [
+        PortMngComponent,
+        PortMngSetComponent
+    ],
+    exports: [
+        PortMngComponent,
+        PortMngSetComponent
+    ],
+    providers: [
+        PortMngService,
+        PortMngSetService
+    ]
+
+})
+export class VMPortMngModule { }
