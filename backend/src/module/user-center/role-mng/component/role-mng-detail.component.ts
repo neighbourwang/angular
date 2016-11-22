c7eef942fc0ebc95325ac007f779ca6db0d4ae68
zhangjunjie <865524326@qq.com>
2016-11-22 15:50:55 +0800

    后台 基本完成

--- backend/src/module/user-center/role-mng/component/role-mng-detail.component.ts
+++ backend/src/module/user-center/role-mng/component/role-mng-detail.component.ts
@@ -16,16 +16,32 @@ export class RoleMngDetailComponent implements OnInit{
         private service : RoleMngService
     ){}
 
+    roleTree : any;
+
+    roleDetail : any;
+
+
+    options = { isExpandedField: 'false' }
+    nodes = []
+
     ngOnInit(){
         this.route.params.forEach((params: Params) => {
             console.log(params['id']);
             this.service.getRoleTree().then(
               res => {
                 console.log('getRoleTree',res);
+  
+                this.roleTree = res.resultContent;
 
                 this.service.getRoleDetail(params['id']).then(
                   res => {
                     console.log('getRoleDetail',res);
+
+                    this.roleDetail = res.resultContent.roleName;
+                    // this.roleDetailTree = res.resultContent.menus;
+                    this.nodes = res.resultContent.menus;
+
+                    console.log(this.nodes);
                   }
                 ).catch(
                   err => {
@@ -41,32 +57,5 @@ export class RoleMngDetailComponent implements OnInit{
         });
     }
 
-    options = { isExpandedField: 'false' }
-    nodes = [
-    {
-      name : '运营运维管理门户',
-      children : [
-        { name : '平台管理' , checked : 'checked'},
-        { name : '企业管理' , checked : 'checked'},
-        { name : '产品管理' , checked : 'checked'},
-        { name : '运营中心' , checked : 'checked'},
-        { 
-          name : '用户中心' ,
-          children : [
-            { name : '帐号管理' , checked : 'checked'},
-            { name : '组织管理' , checked : 'checked'},
-            { 
-              name : '角色管理',
-              children : [
-                { name : '角色管理' , checked : 'checked'},
-                { name : '查看角色' , checked : 'checked'}
-              ]
-            },
-            { name : '认证管理' },
-            { name : '个人账户管理' }
-          ]
-        },
-        { name : '审批中心' }
-      ]
-    }]
+    
 } 
