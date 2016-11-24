e53b3710fe7c9e8bfb0fd0e5c5494a5d00d31c65
neighbourwang <yao-yao.wang@hpe.com>
2016-11-24 17:16:12 +0800

    add account,model

---
+++ frontend/src/module/user-center/account-mng/model/account.model.ts
@@ -0,0 +1,73 @@
+class Org {
+  "key": string;
+  "num": 0;
+  "value": string
+}
+class Rolee {
+  "key": string;
+  "num": boolean;
+  "value": string;
+}
+class Member {
+  "description": string;
+  "email": string;
+  "id": string;
+  "isAD": 0;
+  "loginName": string;
+  "name": string;
+  "orgs": Array<Org>;
+  "phone": string;
+  "roles": Array<Rolee>
+}
+class Platform {
+  "dataCenter": string;
+  "description": string;
+  "id": string;
+  "name": string;
+  "passwd": string;
+  "platformType": boolean;
+  "platformTypeName": "Openstack";
+  "regionId": string;
+  "supportChange": boolean;
+  "uri": "https://xxx.xxx.xxx.xxx";
+  "userName": string;
+  "version": string
+}
+
+class Organization {
+  "description": string;
+  "id": string;
+  "leaderId": string;
+  "members": Array<Member>;
+  "name": string;
+  "platforms": Array<Platform>
+}
+class Role {
+  "description": string;
+  "id": string;
+  "name": string;
+  selected: boolean
+}
+class Account {
+  "description": string;
+  "email": string;
+  "id": string;
+  "isLeader": boolean;
+  "loginName": string;
+  "organizations": Array<Organization>;
+  "phone": string;
+  "roles": Array<Role>;
+  "userName": string;
+  constructor() {
+    this.roles = [{
+      "description": '',
+      "id": '',
+      "name": '',
+      selected: false
+    }]
+  }
+}
+export {
+  Account,
+  Role
+}
