9738513958ccc9af6903325e4f437567a04a2d72
neighbourwang <yao-yao.wang@hpe.com>
2016-11-24 17:15:49 +0800

    add account,model

--- frontend/src/module/user-center/account-mng/component/account-mng-cr-local.component.ts
+++ frontend/src/module/user-center/account-mng/component/account-mng-cr-local.component.ts
@@ -1,11 +1,11 @@
-import { Component,OnInit,Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { LayoutService, NoticeComponent, ConfirmComponent} from '../../../../architecture';
+import { LayoutService, NoticeComponent, ConfirmComponent } from '../../../../architecture';
 
 import { AccountMngService } from '../service/account-mng.service';
 
-import { Account,Role } from '../model/account.model'
+import { Account, Role } from '../model/account.model'
 
 @Component({
   selector: 'account-mng-cr-local',
@@ -14,23 +14,36 @@ import { Account,Role } from '../model/account.model'
   providers: []
 })
 export class AccountMngCrLocalComponent implements OnInit {
-  
+
   constructor(
     private layoutService: LayoutService,
     private router: Router,
-    private service : AccountMngService
-  ) {}
+    private service: AccountMngService
+  ) { }
 
   @Input()
-  isEdit : boolean;
-
+  isEdit: boolean;
+  account = new Account();
+  roles: Array<Role>;
   ngOnInit() {
     this.service.getRoleList().then(
       res => {
         console.log(res);
-        
+        this.roles = res.resultContent;
       }
-    )
+    ).catch(err => {
+      console.error(err);
+    })
   }
-  
+  //选择角色
+  selectRole(idx) {
+    console.log(idx);
+    this.roles[idx].selected = !this.roles[idx].selected;
+    this.account.roles = this.roles.filter(ele => {
+      if (ele.selected) {
+        return ele;
+      }
+    })
+    console.log(this.account.roles);
+  }
 }
