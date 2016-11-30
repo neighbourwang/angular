a87aea9bd34b4a9a4eccd4921aff6aeedd594067
LilyWei <xiao-li.wei@hpe.com>
2016-11-30 18:20:41 +0800

    vmware-mng-dbt wip

--- backend/src/module/net-mng/vm-mng-dbt/vm-mng-dbt.module.ts
+++ backend/src/module/net-mng/vm-mng-dbt/vm-mng-dbt.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 
 import { CommonComponentModule } from '../../../architecture';
+import { IpMngModule } from './ip-mng/ip-mng.module';
 
 @NgModule({
     imports:[
         CommonComponentModule,
+        IpMngModule
     ],
     declarations:[],
-    exports:[],
+    exports:[
+        IpMngModule
+    ],
     providers:[]
 
 })
