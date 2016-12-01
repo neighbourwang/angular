ea6bc1fe7ef7c22281aec4fdbd1b1884954789ae
zhonghui <zhonghui@b5m.com>
2016-12-01 15:29:03 +0800

    up

--- frontend/src/architecture/environments/environment.ts
+++ frontend/src/architecture/environments/environment.ts
@@ -3,22 +3,22 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
-$.ajax({
-    url: "http://15.114.100.52:30072/uaa/oauth/token?grant_type=password&username=gavin@hpe.com&password=12345&client_id=ui&client_secret=12345",
-    type: "POST",
-    beforeSend: function (request)
-    {
-        request.setRequestHeader("Authorization", "Basic " + btoa("ui:secret"))
-    },
-    crossDomain: true,
-    success: function (response) {
-        var resp = JSON.parse(response)
-        alert(resp.status);
-    },
-    error: function (xhr, status) {
-        alert("error");
-    }
-});
+// $.ajax({
+//     url: "http://15.114.100.52:30072/uaa/oauth/token?grant_type=password&username=gavin@hpe.com&password=12345&client_id=ui&client_secret=12345",
+//     type: "POST",
+//     beforeSend: function (request)
+//     {
+//         request.setRequestHeader("Authorization", "Basic " + btoa("ui:secret"))
+//     },
+//     crossDomain: true,
+//     success: function (response) {
+//         var resp = JSON.parse(response)
+//         alert(resp.status);
+//     },
+//     error: function (xhr, status) {
+//         alert("error");
+//     }
+// });
 
 export const environment = {
 	production: false,
