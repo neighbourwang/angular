715d28a2c8851e92af1fa4bf3c43cdff3e84c8cd
Chuyu Guo <chuyu.guo@hpe.com>
2016-12-01 18:32:35 +0800

    gengxin

--- backend/src/module/net-mng/vm-mng/vm-mng/component/vmware-std-net.component.ts
+++ backend/src/module/net-mng/vm-mng/vm-mng/component/vmware-std-net.component.ts
@@ -199,6 +199,16 @@ export class VmwareStdNetComponent implements OnInit {
             this.showAlert("VLAN ID不能为空.");
             return;
         }
+        if (!this.validationService.isNumber(this.tempEditNet.vlanId)) {
+            this.showAlert("VLAN ID必须是数字.");
+            return;
+        }
+        //else {
+        //    if (!(this.tempEditNet.vlanId > 0 && this.tempEditNet.vlanId < 4096)) {
+        //        this.showAlert("VLAN ID必须是1~4095的数字.");
+        //        return;
+        //    }
+        //}
         this.service.saveEditNet(this.tempEditNet)
             .then(
             response => {
