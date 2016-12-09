50baa063807abbec22b5bb2618074805178ddda6
Chuyu Guo <chuyu.guo@hpe.com>
2016-12-09 13:12:24 +0800

    mng-console

---
+++ frontend/src/module/mng-console/mng-console.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+
+// common
+
+import { CommonComponentModule } from '../../architecture';
+
+//routing
+
+import { MngConsoleRouting } from './mng-console.routing';
+
+//component
+import { MngConsoleComponent } from './component/mng-console.component';
+
+
+
+//service
+
+import { MngConsoleService } from './service/mng-console.service'
+
+@NgModule({
+    imports: [
+        MngConsoleRouting,
+        CommonComponentModule
+      
+    ],
+    declarations: [
+        MngConsoleComponent
+    ],
+    exports: [
+    ],
+    providers: [
+        MngConsoleService
+    ]
+
+})
+export class MngConsoleModule { }
