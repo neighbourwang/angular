7daf4979a1a2079518aed07d8af15d546524d299
zhonghui <zhonghui@b5m.com>
2016-12-12 10:40:39 +0800

    set username,password out off url

--- backend/src/architecture/environments/environment.ts
+++ backend/src/architecture/environments/environment.ts
@@ -3,23 +3,26 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
- const promise = new Promise((resolve,reject) => {
-     $.ajax({
-        url: "http://15.114.100.52:32072/uaa/oauth/token?grant_type=password&username=gavin@hpe.com&password=12345&client_id=ui&client_secret=12345",
-        type: "POST",
-        beforeSend: function (request)
-        {
-            request.setRequestHeader("Authorization", "Basic " + btoa("ui:secret"))
-        },
-        crossDomain: true,
-        success: function (response) {
-            resolve("bearer " + response.access_token)
-        },
-        error: function (xhr, status) {
-           reject("获取token失败！")
-        }
-    }); 
- });
+const userName = "gavin@hpe.com",
+      password = "12345"
+
+const promise = new Promise((resolve,reject) => {
+ $.ajax({
+    url: `http://15.114.100.52:32072/uaa/oauth/token?grant_type=password&username=${userName}&password=${password}&client_id=ui&client_secret=12345`,
+    type: "POST",
+    beforeSend: function (request)
+    {
+        request.setRequestHeader("Authorization", "Basic " + btoa("ui:secret"))
+    },
+    crossDomain: true,
+    success: function (response) {
+        resolve("bearer " + response.access_token)
+    },
+    error: function (xhr, status) {
+       reject("获取token失败！")
+    }
+}); 
+});
 
  function ajax(url, token, method?){
       return new Promise((resolve, reject) => {
