b4ab43651ab51fed171554dbdc9d04c7b6a41a15
zhonghui <zhonghui@b5m.com>
2016-12-13 18:58:09 +0800

    一键发布环境制作完毕

--- fis-conf.js
+++ fis-conf.js
@@ -1,7 +1,38 @@
-fis.match('test.js', {
-  deploy: fis.plugin('http-push', {
-    receiver: 'http://16.187.145.3:8999/receiver',
-    //远端目录
-    to: '/etc/nginx/foxcloud/test/'
-  })
-})
+fis.set('project.ignore', ['*/node_modules/**','node_modules/**','fis-conf.js', '*/e2e/**', '*/src/**']);
+
+
+//后台发布到开发环境
+fis.media('backend:dev').match(/(?:backend\/dist\/(.*)\.*)|(?:backend-portal\/dist\/(.*)\.*)|(?:backend-portal(\/public\/.*)?\.*)/i, {
+	release: '/$1$2$3',
+	deploy: fis.plugin('http-push', {
+		receiver: 'http://16.187.145.3:8999/receiver',
+		to: '/etc/nginx/foxcloud/mpp'
+	})
+});
+
+//前台发布到开发环境
+fis.media('frontend:dev').match(/(?:frontend\/dist\/(.*)\.*)|(?:frontend-portal\/dist\/(.*)\.*)|(?:frontend-portal(\/public\/.*)?\.*)/i, {
+	release: '/$1$2$3',
+	deploy: fis.plugin('http-push', {
+		receiver: 'http://16.187.145.3:8999/receiver',
+		to: '/etc/nginx/foxcloud/adm'
+	})
+});
+
+//前台发布到测试环境
+fis.media('backend:test').match(/(?:backend\/dist\/(.*)\.*)|(?:backend-portal\/dist\/(.*)\.*)|(?:backend-portal(\/public\/.*)?\.*)/i, {
+	release: '/$1$2$3',
+	deploy: fis.plugin('http-push', {
+		receiver: 'http://16.187.145.3:8999/receiver',
+		to: '/etc/nginx/foxcloud/frontend'
+	})
+});
+
+//后台发布到测试环境
+fis.media('frontend:test').match(/(?:frontend\/dist\/(.*)\.*)|(?:frontend-portal\/dist\/(.*)\.*)|(?:frontend-portal(\/public\/.*)?\.*)/i, {
+	release: '/$1$2$3',
+	deploy: fis.plugin('http-push', {
+		receiver: 'http://16.187.145.3:8999/receiver',
+		to: '/etc/nginx/foxcloud/backend'
+	})
+});
