7680905bd4959b2d6bb88adc4fc8b7504da63959
zhonghui <zhonghui@b5m.com>
2016-12-13 19:45:23 +0800

    修改window下的问题

--- fis-conf.js
+++ fis-conf.js
@@ -2,7 +2,7 @@ fis.set('project.ignore', ['*/node_modules/**','node_modules/**','fis-conf.js', 
 
 
 //后台发布到开发环境
-fis.media('backend:dev').match(/(?:backend\/dist\/(.*)\.*)|(?:backend-portal\/dist\/(.*)\.*)|(?:backend-portal(\/public\/.*)?\.*)/i, {
+fis.media('backendDev').match(/(?:backend\/dist\/(.*)\.*)|(?:backend-portal\/dist\/(.*)\.*)|(?:backend-portal(\/public\/.*)?\.*)/i, {
 	release: '/$1$2$3',
 	deploy: fis.plugin('http-push', {
 		receiver: 'http://16.187.145.3:8999/receiver',
@@ -11,7 +11,7 @@ fis.media('backend:dev').match(/(?:backend\/dist\/(.*)\.*)|(?:backend-portal\/di
 });
 
 //前台发布到开发环境
-fis.media('frontend:dev').match(/(?:frontend\/dist\/(.*)\.*)|(?:frontend-portal\/dist\/(.*)\.*)|(?:frontend-portal(\/public\/.*)?\.*)/i, {
+fis.media('frontendDev').match(/(?:frontend\/dist\/(.*)\.*)|(?:frontend-portal\/dist\/(.*)\.*)|(?:frontend-portal(\/public\/.*)?\.*)/i, {
 	release: '/$1$2$3',
 	deploy: fis.plugin('http-push', {
 		receiver: 'http://16.187.145.3:8999/receiver',
@@ -20,7 +20,7 @@ fis.media('frontend:dev').match(/(?:frontend\/dist\/(.*)\.*)|(?:frontend-portal\
 });
 
 //前台发布到测试环境
-fis.media('backend:test').match(/(?:backend\/dist\/(.*)\.*)|(?:backend-portal\/dist\/(.*)\.*)|(?:backend-portal(\/public\/.*)?\.*)/i, {
+fis.media('backendTest').match(/(?:backend\/dist\/(.*)\.*)|(?:backend-portal\/dist\/(.*)\.*)|(?:backend-portal(\/public\/.*)?\.*)/i, {
 	release: '/$1$2$3',
 	deploy: fis.plugin('http-push', {
 		receiver: 'http://16.187.145.3:8999/receiver',
@@ -29,7 +29,7 @@ fis.media('backend:test').match(/(?:backend\/dist\/(.*)\.*)|(?:backend-portal\/d
 });
 
 //后台发布到测试环境
-fis.media('frontend:test').match(/(?:frontend\/dist\/(.*)\.*)|(?:frontend-portal\/dist\/(.*)\.*)|(?:frontend-portal(\/public\/.*)?\.*)/i, {
+fis.media('frontendTest').match(/(?:frontend\/dist\/(.*)\.*)|(?:frontend-portal\/dist\/(.*)\.*)|(?:frontend-portal(\/public\/.*)?\.*)/i, {
 	release: '/$1$2$3',
 	deploy: fis.plugin('http-push', {
 		receiver: 'http://16.187.145.3:8999/receiver',
