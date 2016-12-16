4af0c615f6209c1999d07cd106873b1cc271aedc
zhonghui <zhonghui@b5m.com>
2016-12-16 13:59:38 +0800

    修改环境配置

---
+++ backend/src/architecture/environments/environment.boeprod.ts
@@ -0,0 +1,13 @@
+//boe的生产环境
+
+ const promise = new Promise((resolve,reject) => {
+     const token =  window.sessionStorage["token"];
+     token ?　resolve(token) : reject("获取token失败！");
+ })
+ 
+export const environment = {
+    production: true ,
+    baseIp : '10.1.8.142',
+    basePort : '30072',
+    jwt : promise
+};
--- backend/src/architecture/environments/environment.custom.ts
+++
@@ -1,8 +0,0 @@
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-
-export const environment = {
-	production: true ,
-	baseIp : '10.1.8.142',
-	basePort : '30072',
-	jwt : new Promise(next => { next("bearer 0c95311a-db7d-4d36-b12c-0a6fc37109bc") })
-};
