0c22f4d46d699c929314805ebf6a9102903d63a6
LilyWei <xiao-li.wei@hpe.com>
2016-12-21 09:47:56 +0800

    start vmware-nsx-net

--- backend/src/module/net-mng/net-mng.module.ts
+++ backend/src/module/net-mng/net-mng.module.ts
@@ -4,19 +4,22 @@ import { OpenstackModule } from './openstack/openstack.module';
 import { IpMngModule } from './vm-mng/ip-mng/ip-mng.module';
 import { VMMngModule } from './vm-mng/vm-mng.module';
 import { VmMngDbtModule } from './vm-mng-dbt/vm-mng-dbt.module';
+import { VmMngNsxModule } from './vm-mng-nsx/vm-mng-nsx.module';
 @NgModule({
     imports: [
         OpenstackModule,
         IpMngModule,
         VMMngModule,
-        VmMngDbtModule
+        VmMngDbtModule,
+        VmMngNsxModule
     ],
     declarations: [],
     exports: [
         OpenstackModule,
         IpMngModule,
         VMMngModule,
-        VmMngDbtModule
+        VmMngDbtModule,
+        VmMngNsxModule
     ],
     providers: []
 })
