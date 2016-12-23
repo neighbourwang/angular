c7abce2c7c49c0801838c2c443ee96bf127b5b6c
LilyWei <xiao-li.wei@hpe.com>
2016-12-23 15:01:24 +0800

    add dict file for multi-lanage in ent-admin, ent-attest and user-center.attest-mng.

---
+++ backend/src/module/ent-mng/ent-attest-mng/translateCN.ts
@@ -0,0 +1,69 @@
+export const TranslateCN = {
+    LANG: '中文',
+    SITE: {
+        EXIT: '退出'
+    },
+    DIALOG: {
+        PROMPT: '提示',
+        CONFIRM: '确认',
+        CANCEL: '取消'
+    },
+    ENT_ATTEST_MNG: {
+        ENSURE: '确定',
+        CONFIRM: '确认',
+        SAVE: '保存',
+        CANCEL: '取消',
+        RETURN: '返回',
+        SYNC: '同步',
+        CREATE: '创建',
+        CHOOSE: '选择',
+        EDIT: '编辑',
+        ENABLE: '启用',
+        DISABLE: '禁用',
+        DELETE: '删除',
+
+        STATUS: '状态',
+        DESCRIPTION: '描述',
+        PROMPT: '提示',
+        ALERT: '警告',
+        GETTING_DATA_FAILED: '获取数据失败！',
+        
+
+        AUTHENTICATION_MNG: '认证管理',
+        RETURN_TO_ENT_MNG: '返回企业管理列表',
+        MORE_OPERATION: '更多操作',        
+        UPDATE_AUTH_ACCOUNT: '修改认证账户',        
+        CREATE_AUTHENTICATION: '创建认证源',
+        
+        AUTHENTICATION_NAME: '认证源名称',
+        URL_ADDRESS: 'URL地址',
+        AUTH_TYPE: '认证类型',
+        
+
+        USER_NAME: '用户名',        
+        PWD: '密码',
+        AUTHENTICATION: '认证源',
+        TEST: '测 试',
+        AD_TEST_PASSED: 'AD认证测试通过',
+        AD_TEST_FIALED: 'AD认证测试未通过',
+        LOGIN_USER_PROPERTY: '登录账户名属性',
+
+        PLEASE_CHOOSE_AUTH_TO_EDIT: '请先选择需要编辑的认证源',
+        PLEASE_CHOOSE_AUTH_TO_ENABLE_DISABLE: '请先选择需要启用或者禁用的认证源',
+        AUTH_ALREADY_STATUS: '该认证源已经是{{status}}状态！',
+        PLEASE_CHOOSE_AUTH_TO_DELETE: '请先选择需要删除的认证源',
+        CONFIRM_TO_DELETE_AUTH: '确认删除认证源{{auth}} ?',
+
+        EDIT_AUTH: '编辑认证源',
+        CREATE_AUTH: '创建认证源',
+        REQUIRED_AUTH: '必须先填写认证源名称！',
+        REQUIRED_URL: '必须先填写URL地址！',
+        
+        REQUIRED_USER_NAME: '必须先填写用户名！',
+        REQUIRED_EMAIL: '用户名必须是邮箱！',
+        REQUIRED_PWD: '必须先填写密码！',
+        REQUIRED_LOGIN_USER_PROPERTY: '必须先填写登录账户名属性！',
+        SAVE_SUCCESS: '保存成功！',
+
+    }
+}
