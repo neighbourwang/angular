49bf773b81952c46030628d6c6effbea79cfb72c
yuan-wei <yuan.wei@hpe.com>
2016-12-26 17:00:27 +0800

    '添加弹出框翻译'

--- backend/src/architecture/components/dialog/component/confirm.component.ts
+++ backend/src/architecture/components/dialog/component/confirm.component.ts
@@ -1,11 +1,13 @@
 ﻿import { Component, ViewChild, EventEmitter, Input, Output, OnInit } from "@angular/core";
 
 import { ModalComponent } from "ng2-bs3-modal/ng2-bs3-modal";
+import { DialogTranslate } from '../service/dialog-translate.service';
 
 @Component({
     selector: "fc-confirm",
     templateUrl: "../template/confirm.component.html",
-    inputs: ["title", "msg", "ot", "ct"]
+    inputs: ["title", "msg", "ot", "ct"],
+    providers: [DialogTranslate]
 })
 export class ConfirmComponent implements OnInit {
     @Output()
@@ -13,13 +15,23 @@ export class ConfirmComponent implements OnInit {
     @Output()
     cf = new EventEmitter<any>();
 
+    param = { value: <String>'' };
     title: String;
     msg: String;
+    
+    constructor(private dialogTranslate: DialogTranslate) {
+        
+    }
 
     @ViewChild("dialog")
     private dialog: ModalComponent;
 
     ngOnInit() {
+    }
+
+    ngOnChanges() {
+        this.param.value = this.dialogTranslate.getParam(this.msg);
+        this.msg = this.dialogTranslate.getText(this.msg);
     }
 
     cof() {
@@ -33,6 +45,8 @@ export class ConfirmComponent implements OnInit {
     open(title?: String, msg?: String) {
         title && (this.title = title);
         msg && (this.msg = msg);
+        this.param.value = this.dialogTranslate.getParam(this.msg);
+        this.msg = this.dialogTranslate.getText(this.msg);
 
         this.dialog.open();
     }
