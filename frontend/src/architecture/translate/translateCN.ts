8cca27434309b0e166f35b1d7be6cc3649adde55
Chuyu Guo <chuyu.guo@hpe.com>
2016-12-26 12:39:02 +0800

    check-center 多语言

--- frontend/src/architecture/translate/translateCN.ts
+++ frontend/src/architecture/translate/translateCN.ts
@@ -1,12 +1,76 @@
 export const TranslateCN = {
     LANG: '中文',
+    COMMON: {
+        PROMPT: '提示',
+        CONFIRM: '确认',
+        CANCEL: '取消',
+        EXIT: '退出',
+        RESET: '重置',
+        NUMBER: '编号',
+        OPERATION: '操作',
+        TYPE: '类型',
+        DESCRIPTION: '描述',
+        ORDER: '订购',
+        OVERVIEW: '概览',
+        LABEL: '标签',
+        EXPENSE: '费用',
+        ONE_TIME_COSTS: '一次性费用',
+        TOTAL_COST: '费用总计',
+        BY_VOLUMN: '按量',
+        DETAILS: '详情',
+        BUY_NOW: '立即购买',
+        SHOPPING_CART: '购物车',
+        ADD_TO_SHOPPING_CART: '加入购物车',
+        CHECK_SHOPPING_CART: '查看购物车',
+        EMPTY_SHOPPING_CART: '清空购物车',
+        NAME: '名称',
+        NONE: '无',
+        ALL: '所有',
+        //单位
+        $: "￥",
+
+        //状态名词
+        ENABLED: '已启用',
+        NOT_ENABLED: '未启用',
+        NOT_APPROVED: '未审批',
+        APPROVED: '已审批',
+        NEWLY_CREATED: '新创建',
+        IN_PROCESS: '处理中',
+        COMPLETED: '已完成',
+
+        //专业术语
+        MIRROR: '镜像',
+        CLOUD_HOSTING: '云主机',
+        CLOUD_HOST_NAME: '云主机名称',
+        CLOUD_HARD_DISK: '云硬盘',
+        CLOUD_HRAD_DISK_NAME: '云硬盘名称',
+        ENTER_CLOUD_HARD_DISK_INFORMATION_TO_SEARCH: '输入云硬盘信息搜索',
+        CLOUD_PLATFORM: '云平台',
+        RAM: '内存',
+        CONFIGURATION: '配置',
+        STORAGE: '存储',
+        STORAGE_TYPE: '储存类型',
+        SNAPSHOT: '快照',
+        CONSOLE: '控制台',
+        PHYSICAL_MACHINE: '物理机',
+        FLOAT: '浮动',
+        PATH: '地址',
+        SERVICES: '服务',
+        INSTANCE: '实例',
+        CAPATITY: '容量',
+        CAPACITY_GB: '容量(GB)',
+        MOUNT: '挂载',
+        AVAILABLE_ZONE: '可用区',
+        PURCHASE_AMOUNT: '购买量',
+    },
     SITE: {
         EXIT: '退出'
     },
     DIALOG: {
         PROMPT: '提示',
         CONFIRM: '确认',
-        CANCEL: '取消'
+        CANCEL: '取消',
+        COMMIT: '提交'
     },
     MENU: {
         CLOUD_HOSTING_SERVICES: '云主机服务',
@@ -106,5 +170,101 @@ export const TranslateCN = {
         YOU_CHOOSE_TO_ENABLE_VALUE_PLEASE_CONFIRM: '您选择启用{{value}}，请确认',
         EDIT_DEPARTMENT: '编辑部门',
         YOU_CAN_NOT_DELETE_ORGANIZATIONS_THAT_ARE_ENABLED: '不能删除启用状态下的组织'
+    },
+    CHECK_CENTER: {
+        //check-mng-list.html
+        PENDING_LIST: '待审批列表',
+        ENTER_THE_ORDER_NUMBER_OR_DEPARTMENT_FOR_QUICK_INQUIRY: '输入订单号或部门快速查询',
+        ADVANCED_SEARCH: '高级查询',
+        ORDER_ATTRIBUTION: '订单归属',
+        ALL_DEPARTMENTS: '所有部门',
+        SUBMITTER: '提交者',
+        ALL_SUBMITTERS: '所有提交者',
+        ORDER_TYPE: '订单类型',
+        ALL_TYPES: '所有类型',
+        PRODUCT_TYPE: '产品类型',
+        CREATED_TIME: '创建时间',
+        QUERY: '查  询',
+        RESET: '重置',
+        CURRENT_QUERY_IS: '当前查询到',
+        THE_QUERY_RESULTS_OF_PENDING_ORDERS: '个订单需要审批，查询结果如下',
+        NUMBER: '编号',
+        ORDER_NUMBER: '订单号',
+        REGION_AND_AVAIABLE_AREA: '所在区域及可用区',
+        ORDER_DESCRIPTION: '订单描述',
+        DEPARTMENT: '部门',
+        COST: '费用',
+        APPROVAL_RESULTS: '审批结果',
+        BILLING_MODE: '计费模式',
+        ORDER_CYCLE: '订单周期',
+        ONE_TIME_COSTS: '一次性费用',
+        AGREE: '同意',
+        REFUSE: '拒绝',
+        THE_ORDER_REQUEST_WAS_REJECTED: '订单请求被拒绝',
+        REASON: '原因',
+        INPUT_CHINESE_OR_CHARACTERS_NOT_MORE_THAN_200_WORDS: '可以输入中文与字符,最大不能超过200个字',
+
+        //check-mng-hascheck.html
+        APPROVED_LIST: '已审批列表',
+        APPROVER: '审批人',
+        ALL_APPPOVERS: '所有审批人',
+        APPROVAL_TIME: '审批时间',
+        APPROVAL_COMMENTS: '审批意见'
+    },
+
+    //cloud-drive文件夹下
+    CLOUD_DRIVE_LIST: {
+        AREA_LIST: '区域列表',
+        STATUS: '状态',
+
+        ADVANECD_SEARCH: '高级搜索',
+        RESET_SEARCH: '重置搜索',
+        CREATE_CLOUD_HRAD_DISK: '创建云硬盘',
+        CLOUD_HRAD_DISK_NAME: '云硬盘名称',
+        MOUNT_LOCATION_CLOUD_HOST_NAME: '挂载位置（云主机名称）',
+        EXPIRE_DATE: '到期时间',
+        REGION: '所在地区',
+        AVAILABLE_AREA: '可用区',
+        ATTRIBUTE: '属性',
+        TYPE: '类型',
+        OPERATE: '操作',
+        MORE_ACTIONS: '更多操作',
+        MOUNT: '挂载',
+        UNINSTALL: '卸载',
+        MOUNT_CLOUD_HOST: '挂载云主机',
+    },
+    CLOUD_DRIVE_ORDER: {
+        AREA: '区域',
+        //28行：云硬盘只能挂载到同一可用区下的云主机实例，订购成功后不支持更换云硬盘的区域和可用区；请谨慎选择
+        CONFIGURATION_OPTIONS: '配置选项',
+        BLANK_DISK: '空白盘',
+        CLOUD_HARD_DISK_TYPE: '云硬盘类型',
+        //73行
+        CLOUD_HARD_DISK_INSTANCE_NAME: '云硬盘实例名称',
+        //81行
+        WHETHER_TO_AMOUNT_ON_CLOUD_HOST: '是否挂载到云主机',
+        CLOUD_HOST_INSTANCE_LIST: '云主机实例列表',
+        ONE_SET: '1台',
+        TWO_SET: '2台',
+        THREE_SET: '3台',
+        FOUR_SET: '4台',
+        FIVE_SET: '5台',
+        CURRENT_CONFIGURATION: '当前配置',
+        CLOUD_HRAD_DISK_TYPE: '云硬盘类型',
+        MOUNT_TO_CLOUD_HOST: '挂载到云主机',
+    },
+    SUB_TABLE_LIST: {
+        BACKUP_NAME: '备份名称',
+        SOURCE_CLOUD_HARD_DISK_NAME: '源云硬盘名称',
+        SOURCE_VIRTUAL_MACHINE_NAME: '源虚拟机名称',
+        ENTER_INFORMATION_TO_SEARCH: '输入信息搜索',
+        INTERNAL_IP: '内部IP',
+        EXTERNAL_IP: '外部IP'
+    },
+
+    //shopping-cart文件夹下
+    SHOPPING_CART: {
+        PRODUCT_INFORMATION: '产品信息',
+        PRODUCT_TYPE:'产品类型',
     }
 }
