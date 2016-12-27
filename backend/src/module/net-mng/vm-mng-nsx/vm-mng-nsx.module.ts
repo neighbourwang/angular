25de65d8f98f3f6392452ca3ca1b6269e601228e
Chuyu Guo <chuyu.guo@hpe.com>
2016-12-27 18:14:21 +0800

    vmware nsx

--- backend/src/module/net-mng/vm-mng-nsx/vm-mng-nsx.module.ts
+++ backend/src/module/net-mng/vm-mng-nsx/vm-mng-nsx.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 
 import { CommonComponentModule } from '../../../architecture';
 import { IpMngModule } from './ip-mng/ip-mng.module';
-
+import { VmNSXIndexModule } from './index-nsx/index-nsx.module';
 @NgModule({
     imports:[
         CommonComponentModule,
-        IpMngModule
+        IpMngModule,
+        VmNSXIndexModule
     ],
     declarations:[],
     
     exports: [
         IpMngModule,
+        VmNSXIndexModule
     ],
     providers:[]
 
