d2dc55b2e7617a07b936abb5248a53b0d8cffc8b
LilyWei <xiao-li.wei@hpe.com>
2016-12-29 20:41:51 +0800

    update for vmware-net-overview-page.

--- backend/src/module/net-mng/vm-mng-index/component/vm-mng-index.component.ts
+++ backend/src/module/net-mng/vm-mng-index/component/vm-mng-index.component.ts
@@ -3,7 +3,14 @@ import { Router } from '@angular/router';
 import { RestApi, RestApiCfg, LayoutService,PopupComponent, NoticeComponent, ValidationService, 
     ConfirmComponent, SystemDictionary } from '../../../../architecture';
 
-import { VmwareNetModel } from '../model/vmware-net.model';
+//Mock
+import { RegionInfo_mock } from '../model/vmware-net.mock';
+
+//Model
+import { PlatformModel, DCModel, RegionModel, VmwareNetModel, NsxNetModel } from '../model/vmware-net.model';
+
+//Service
+import { VmwareMngIndexService } from '../service/vm-mng-index.service';
 
 @Component({
     selector: "vmware-net-mng-index",
@@ -18,6 +25,7 @@ export class VmwareMngIndexComponent implements OnInit {
         private router: Router,
         private layoutService: LayoutService,
         private validationService: ValidationService,
+        private service: VmwareMngIndexService
     ) {
     }
     @ViewChild("notice")
@@ -26,9 +34,6 @@ export class VmwareMngIndexComponent implements OnInit {
     @ViewChild("confirm")
     confirm: ConfirmComponent;
 
-    @ViewChild("synEnts")
-    synEnts: PopupComponent;
-
     noticeTitle = "";
     noticeMsg = "";
 
@@ -36,65 +41,217 @@ export class VmwareMngIndexComponent implements OnInit {
     pageSize = 10;
     totalPage = 1;
 
-    
-    ngOnInit() {        
+    regionList: Array<RegionModel> = [];
+
+    defaultRegion: RegionModel = new RegionModel(); 
+    defaultDC: DCModel = new DCModel();
+    defaultPlatform: PlatformModel = new PlatformModel();
+    selectedRegion: RegionModel = this.defaultRegion;
+    selectedDC: DCModel = this.defaultDC;
+    selectedPlatform: PlatformModel = this.defaultPlatform;
+
+    queryOpt: PlatformModel = new PlatformModel();
+
+    NsxInfo: NsxNetModel = new NsxNetModel();
+
+    networkList: Array<VmwareNetModel> = [];
+    selectedNet: VmwareNetModel = new VmwareNetModel();
+
+    private okCallback: Function = null;
+    okClicked() {
+        console.log('okClicked');
+        if (this.okCallback) {
+            console.log('okCallback()');
+            this.okCallback();
+            this.okCallback = null;
+        }
     }
 
+    private confirmedHandler: Function = null;
+    onConfirmed() {
+        if (this.confirmedHandler) {
+            this.confirmedHandler();
+            this.confirmedHandler = null;
+        }
+    }
+
+    
+    ngOnInit() {
+        this.getRegionInfo();
+    }
+
+    getRegionInfo(): void {
+        this.layoutService.show();
+        this.service.getRegionInfo()
+            .then(response => {
+                this.layoutService.hide();
+                if (response && 100 == response["resultCode"]) {                    
+                    this.regionList = response.resultContent;
+                    console.log(this.regionList, "this.regionList");
+                } else {
+                    alert("Res sync error");
+                }
+            }
+            ) .catch((e) => this.onRejected(e));
+    }
+
+    getNsxInfo(platformId:string): void {        
+        this.layoutService.show(platformId);
+        this.service.getNsxInfo()
+            .then(response => {
+                this.layoutService.hide();
+                if (response && 100 == response["resultCode"]) {                    
+                    this.NsxInfo = response.resultContent;
+                    console.log(this.NsxInfo, "this.NsxInfo");
+                } else {
+                    alert("Res sync error");
+                }
+            }
+            ) .catch((e) => this.onRejected(e));
+    }
+
+    getNetworkList(platformId:string): void {
+        //this.pageIndex = pageIndex || this.pageIndex;
+        this.layoutService.show();
+        this.service.getNetworkList(platformId)
+            .then(
+            response => {
+                this.layoutService.hide();
+                if (response && 100 == response["resultCode"]) {
+                    this.layoutService.hide();
+                    this.networkList = response.resultContent;
+                    //this.totalPage = response.pageInfo.totalPage;
+                    console.log(this.networkList, "this.networkList");
+                } else {
+                    alert("Res sync error");
+
+                }
+            }
+            )
+            .catch((e) => this.onRejected(e));
+    }
+
+    search(): void {
+        if (this.queryOpt && !this.validationService.isBlank(this.queryOpt.platformId)){
+            console.log(this.queryOpt.platformId, "this.queryOpt.platformId");
+            this.getNetworkList(this.queryOpt.platformId);
+            this.getNsxInfo(this.queryOpt.platformId);
+        } else {
+            this.showAlert("请选择相应的平台");
+        }
+    }
+
+    
+
     gotoVMStdNetMng() {
-        //const selectedNet = this.filternets.find((stdnet) => { return stdnet.selected })
-        const selectedNet = new VmwareNetModel();
-        selectedNet.dcId = "ef300001-3bfb-4b09-b804-30954b084001";
-        selectedNet.clusterId = "ef349044-3bfb-4b09-b804-30954b084001";
-        selectedNet.platformId = "88"
-        if (selectedNet) {
+        this.selectedNet = this.getSelected();
+        if (this.queryOpt && !this.validationService.isBlank(this.queryOpt.platformId)) {
+        if (this.selectedNet) {
             this.router.navigate([
-                `net-mng/vm-mng/${selectedNet.platformId}`,
+                `net-mng/vm-mng/${this.queryOpt.platformId}`,
                 {
-                    "dc_Id": selectedNet.dcId,
-                    "cls_Id": selectedNet.clusterId
+                    "dc_Id": this.selectedNet.dcId,
+                    "cls_Id": this.selectedNet.clusterId
                 }
                 ]);
         } else {
-            this.router.navigate([`net-mng/vm-mng/${selectedNet.platformId}`]);
+            this.router.navigate([`net-mng/vm-mng/${this.queryOpt.platformId}`]);
+        }
+        } else {
+            this.showAlert("请选择相应的平台");
+
         }
     }
 
     gotoVMDbtNetMng() {
-        //const selectedNet = this.filternets.find((stdnet) => { return stdnet.selected })
-        const selectedNet = new VmwareNetModel();
-        selectedNet.dcId = "ef300001-3bfb-4b09-b804-30954b084001";
-        selectedNet.clusterId = "ef349044-3bfb-4b09-b804-30954b084001";
-        selectedNet.platformId = "88"
-        if (selectedNet) {
+        this.selectedNet = this.getSelected();
+        if (this.queryOpt && !this.validationService.isBlank(this.queryOpt.platformId)) {
+        if (this.selectedNet) {
             this.router.navigate([
-                `net-mng/vm-mng-dbt/index/${selectedNet.platformId}`,
+                `net-mng/vm-mng-dbt/index/${this.queryOpt.platformId}`,
                 {
-                    "dc_Id": selectedNet.dcId,
-                    "cls_Id": selectedNet.clusterId
+                    "dc_Id": this.selectedNet.dcId,
+                    "cls_Id": this.selectedNet.clusterId
                 }
                 ]);
         } else {
-            this.router.navigate([`net-mng/vm-mng-dbt/index/${selectedNet.platformId}`]);
+            this.router.navigate([`net-mng/vm-mng-dbt/index/${this.queryOpt.platformId}`]);
+        }
+        } else {
+            this.showAlert("请选择相应的平台");
+
         }
     }
 
     gotoVMNsxNetMng() {
-        //const selectedNet = this.filternets.find((stdnet) => { return stdnet.selected })
-        const selectedNet = new VmwareNetModel();
-        selectedNet.dcId = "ef300001-3bfb-4b09-b804-30954b084001";
-        selectedNet.clusterId = "ef349044-3bfb-4b09-b804-30954b084001";
-        selectedNet.platformId = "88"
-        if (selectedNet) {
+        this.selectedNet = this.getSelected();
+        if (this.queryOpt && !this.validationService.isBlank(this.queryOpt.platformId)) {
+        if (this.selectedNet) {
             this.router.navigate([
-                `net-mng/vm-mng-nsx/index/${selectedNet.platformId}`,
+                `net-mng/vm-mng-nsx/index/${this.queryOpt.platformId}`,
                 {
-                    "dc_Id": selectedNet.dcId,
-                    "cls_Id": selectedNet.clusterId
+                    "dc_Id": this.selectedNet.dcId,
+                    "cls_Id": this.selectedNet.clusterId
                 }
                 ]);
         } else {
-            this.router.navigate([`net-mng/vm-mng-nsx/index/${selectedNet.platformId}`]);
+            this.router.navigate([`net-mng/vm-mng-nsx/index/${this.queryOpt.platformId}`]);
+        }
+        } else {
+            this.showAlert("请选择相应的平台");
+
         }
     }
+    
+    onRejected(reason: any) {
+        this.layoutService.hide();
+        console.log(reason);
+        this.showAlert("NET_MNG_VM_IP_MNG.GETTING_DATA_FAILED");
+    }
+
+    showMsg(msg: string) {
+        console.log(msg, "showMsg");
+        this.notice.open("NET_MNG_VM_IP_MNG.SYSTEM_PROMPT", msg);
+    }
+
+    showAlert(msg: string): void {
+        this.layoutService.hide();
+        this.noticeTitle = "NET_MNG_VM_IP_MNG.PROMPT";
+        this.noticeMsg = msg;
+        this.notice.open();
+    }
+
+    //根据value显示
+    displayIt(value: any): any {
+        if(this.validationService.isBlank(value)){
+            return "NET_MNG_VM_IP_MNG.UNSET";
+        } else {
+            return value.toString();
+            
+        }
+    }
+
+    //选择行
+    selectItem(index:number): void {
+        this.networkList.map(n=> {n.checked = false;});
+        this.networkList[index].checked = true;
+        console.log(this.networkList[index], "=== Please see which one is selected ===");
+    }
+
+    UnselectItem(): void {
+        this.networkList.map(n=> {n.checked = false;});
+    }
+
+    getSelected() {
+        let item = this.networkList.find((n) => n.checked) as VmwareNetModel;
+        if (item){
+            return item;
+        }
+        else {
+            this.showMsg("NET_MNG_VM_IP_MNG.PLEASE_CHOOSE_PG");
+            return null;
+        }
+    }
+    
 
 }
