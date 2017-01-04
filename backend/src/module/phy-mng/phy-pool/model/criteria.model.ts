14d3600f29091fd7f8151cac917c06016c780de2
Huidan Fan <huidan.fan@hpe.com>
2017-01-04 15:31:13 +0800

    物理机资源池

---
+++ backend/src/module/phy-mng/phy-pool/model/criteria.model.ts
@@ -0,0 +1,13 @@
+export class Criteria{
+    poolName: string;
+    region: string;
+    dataCenter: string;
+}
+/*
+{
+    "criteriaQuery": {
+    "poolName": "北京地区-物理资源池1",
+        "region": "北京",
+        "dataCenter": "朝阳数据中心"
+}
+}*/
