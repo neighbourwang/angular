804e613b883cbceea6714c8c3cfc3548bc049557
yuan-wei <yuan.wei@hpe.com>
2017-01-05 11:24:13 +0800

    '添加弹出框标题带参数'

--- backend/src/architecture/components/dialog/component/popup.component.ts
+++ backend/src/architecture/components/dialog/component/popup.component.ts
@@ -1,11 +1,13 @@
 import { Component, ViewChild, EventEmitter, Input, Output, OnInit } from "@angular/core";
 
 import { ModalComponent } from "ng2-bs3-modal/ng2-bs3-modal";
+import { DialogTranslate } from '../service/dialog-translate.service';
 
 @Component({
     selector: "fc-popup",
     templateUrl: "../template/popup.component.html",
-    inputs: ["title", "ot", "ct"]
+    inputs: ["title", "ot", "ct"],
+    providers: [DialogTranslate]
 })
 export class PopupComponent implements OnInit {
     @Output()
@@ -13,7 +15,13 @@ export class PopupComponent implements OnInit {
     @Output()
     cf = new EventEmitter<any>();
 
+    param: any;
     title: String;
+    showTitle: String;
+
+    constructor(private dialogTranslate: DialogTranslate) {
+
+    }
 
     @ViewChild("dialog")
     private dialog: ModalComponent;
@@ -21,6 +29,10 @@ export class PopupComponent implements OnInit {
     ngOnInit() {
     }
 
+    ngOnChanges() {
+        this.param = this.dialogTranslate.getParam(this.title);
+        this.showTitle = this.dialogTranslate.getText(this.title);
+    }
     cof() {
         this.of.emit();
     }
@@ -31,6 +43,8 @@ export class PopupComponent implements OnInit {
 
     open(title?: String, msg?: String) {
         title && (this.title = title);
+        this.param = this.dialogTranslate.getParam(this.title);
+        this.showTitle = this.dialogTranslate.getText(this.title);
 
         this.dialog.open();
     }
