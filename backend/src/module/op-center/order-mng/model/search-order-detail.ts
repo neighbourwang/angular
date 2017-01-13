05e95853babd2f362a231608495b56b91fdfc977
qing-ren-zi <qing-ren.zi@hpe.com>
2017-01-13 22:01:09 +0800

    临时处理错误

---
+++ backend/src/module/op-center/order-mng/model/search-order-detail.ts
@@ -0,0 +1,12 @@
+class SearchOrderDetail{//订单列表
+	subInstanceList: any;
+    orderInstanceItems : any;
+    type: any;
+}
+class SubInstanceItemResp1 {
+
+}
+export  {
+    SearchOrderDetail,
+    SubInstanceItemResp1
+}
