dc6281f8e4d8678a5ce8b6f852d0b6eee8c8e4ad
YXYFuture <1247676987@qq.com>
2017-02-10 10:30:27 +0800

    前台统计图

--- frontend/src/module/op-center/order-mng/component/cost-pandect.component.ts
+++ frontend/src/module/op-center/order-mng/component/cost-pandect.component.ts
@@ -11,7 +11,128 @@ import { SubInstanceResp, AdminListItem, DepartmentItem, Platform, ProductType, 
 	providers: []
 })
 export class CostPandectComponent implements OnInit{
+	ent_dht:any=[{
+                        data: [25,57,173,200],
+                        backgroundColor: [
+                            "#FFCC33","#2BD2CA","#00CC99","#2BD2CA"
+                        ],
+                        borderWidth:[
+                            0,0,0,0
+                        ]
+                    }];//企业消费概览
 
+
+  d_labels=[
+                        '物理机：'+25,
+                        '数据库：'+ 57,
+                        '云硬盘：'+ 173,
+                        '云主机：'+ 200,
+                    ];
+	ent_bar:any=[{
+                        type: "bar",
+                        label: "总消费",
+                        data: [65, 59, 80, 81, 56, 55, 40],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                         borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ]
+                         
+                    }
+                   ];//消费趋势
+	ent_hbar:any=[{
+                        label:'消费总额',
+                        data: [65, 59, 80, 81, 56],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                        ],
+                         borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)'
+                        ]
+                         
+                    }];//消费总额排名
+	ent_hbar2:any=[{
+                        label:'消费总额',
+                        data: [65, 59, 80, 81, 56],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                        ],
+                         borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)'
+                        ]
+                         
+                    }];//新增消费排名
+
+d_options={
+                    legend: {
+                        position: 'bottom',
+                        display: true,
+                        labels: {
+                            boxWidth: 12
+                        }
+                    },
+                    tooltips: {
+                        enabled: false,
+                    },
+                    cutoutPercentage: 82,
+                }
+bar_options={
+                scales: {
+                    xAxes: [{
+                        display: false
+                    }],
+                    yAxes: [{
+                        stacked: true
+
+                    }]
+                }
+            };
+h_options={
+                    scales: {
+                        xAxes: [{
+                             stacked: true
+                        }],
+                        yAxes: [{
+                            stacked: true
+                        }]
+                    }
+                };
+	
+	h_options2={
+                    scales: {
+                        xAxes: [{
+                             stacked: true
+                        }],
+                        yAxes: [{
+                            stacked: true
+                        }]
+                    }
+                };
+	
+	
 	constructor(
 		private layoutService: LayoutService,
 		private router: Router,
@@ -21,21 +142,6 @@ export class CostPandectComponent implements OnInit{
 	ngOnInit(){
 	
 	}
-
-	//图表的数据
-public quotaDtasets = [{ data: [ 30, 70 ], backgroundColor: [ "#00CC99","#E7E9ED" ], borderWidth:[  0,0  ] }];
-
-public quotaOptions = {
-            legend: { display: false },
-            tooltips: {  enabled: false },
-            cutoutPercentage: 82
-        };
-
-public useDatasets = [{
-                    data: [25,57,173],
-                    backgroundColor: [ "#FFCC33","#FF6666","#00CC99"],
-                    borderWidth:[ 0,0,0]
-                }];
 
 //图表的事件
 public chartClicked(e:any):void {
