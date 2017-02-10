de3a60bc9b18df5eebaf6d5211b205d76c15d885
qing-ren-zi <qing-ren.zi@hpe.com>
2017-02-10 11:44:25 +0800

    修改图标

--- frontend/src/module/op-center/order-mng/component/cost-pandect.component.ts
+++ frontend/src/module/op-center/order-mng/component/cost-pandect.component.ts
@@ -13,14 +13,17 @@ import { SubInstanceResp, AdminListItem, DepartmentItem, Platform, ProductType, 
 export class CostPandectComponent implements OnInit{
 	ent_dht:any=[{
                         data: [25,57,173,200],
-                        backgroundColor: [
-                            "#FFCC33","#2BD2CA","#00CC99","#2BD2CA"
-                        ],
                         borderWidth:[
                             0,0,0,0
                         ]
                     }];//企业消费概览
-
+    lineChartColors:Array<any> = [
+        {
+            backgroundColor: [
+                "#FFCC33","#2BD2CA","#00CC99","#FF6666"
+            ]
+        }
+    ];
 
   d_labels=[
                         '物理机：'+25,
@@ -32,59 +35,82 @@ export class CostPandectComponent implements OnInit{
                         type: "bar",
                         label: "总消费",
                         data: [65, 59, 80, 81, 56, 55, 40],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                         borderColor: [
-                            'rgba(255,99,132,1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ]
                          
-                    }
+                    },{   type: 'line',
+                            label: "新增消费",
+                            fill: false,
+                            lineTension: 0.1,
+                            borderCapStyle: 'butt',
+                            borderDash: [],
+                            borderDashOffset: 0.0,
+                            borderJoinStyle: 'miter',
+                            pointBorderWidth: 1,
+                            pointHoverRadius: 5,
+                            pointHoverBorderWidth: 2,
+                            pointRadius: 1,
+                            pointHitRadius: 10,
+                            data:[65, 59, 80, 81, 56, 55, 40],
+                            spanGaps: false,
+                        }
                    ];//消费趋势
+barColors = [
+    {
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+         borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ]
+    },{
+
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+    }
+];
+
+bar_labels = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月"];
+hbar_labels = ["IT部门", "财务部门", "应用部门", "营销部门", "人事部门"];
+hbar_labels2 = ["IT部门", "财务部门", "应用部门", "营销部门", "人事部门"];
 	ent_hbar:any=[{
                         label:'消费总额',
-                        data: [65, 59, 80, 81, 56],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                        ],
-                         borderColor: [
-                            'rgba(255,99,132,1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)'
-                        ]
+                        data: [65, 59, 80, 81, 56]
                          
                     }];//消费总额排名
 	ent_hbar2:any=[{
                         label:'消费总额',
-                        data: [65, 59, 80, 81, 56],
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                        ],
-                         borderColor: [
-                            'rgba(255,99,132,1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)'
-                        ]
+                        data: [65, 59, 80, 81, 56]
                          
                     }];//新增消费排名
+hbarColors = [
+    {
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+        ],
+         borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)'
+        ]
+    }
+]
 
 d_options={
                     legend: {
