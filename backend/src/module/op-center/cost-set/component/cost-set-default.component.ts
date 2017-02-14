76e965fa808a1cbc6c0b7032ae2d4e1d5278fda5
YXYFuture <1247676987@qq.com>
2017-02-14 10:47:41 +0800

    后台费用设置

---
+++ backend/src/module/op-center/cost-set/component/cost-set-default.component.ts
@@ -0,0 +1,67 @@
+import { Input, Component, OnInit, ViewChild, } from '@angular/core';
+import { Router } from '@angular/router';
+import { NoticeComponent,DicLoader,ItemLoader, RestApi, RestApiCfg, LayoutService, ConfirmComponent } from '../../../../architecture';
+import {} from '../model'
+
+import * as _ from 'underscore';
+
+@Component({
+	selector: 'cost-set-default',
+	templateUrl: '../template/cost-set-default.component.html',
+	styleUrls: ['../style/cost-set-default.less'],
+	providers: []
+})
+export class CostSetDefaultComponent implements OnInit{
+
+@ViewChild("notice")
+  	private _notice: NoticeComponent;
+//订单类型
+private _orderTypeDic:DicLoader = null;
+//订购人
+private _buyerLoader:ItemLoader<{id:string; name:string}> = null;
+
+	
+	constructor(
+		private layoutService: LayoutService,
+		private router: Router,
+		private restApiCfg:RestApiCfg,
+		private restApi:RestApi){
+        //订购人加载
+		this._buyerLoader = new ItemLoader<{id:string; name:string}>(false, 'ORDER_MNG.SUBSCRIBER_LIST_DATA_FAILED', "check-center.submiter-list.get", this.restApiCfg, this.restApi);
+
+        this._buyerLoader.MapFunc = (source:Array<any>, target:Array<{id:string;name:string}>)=>{
+			for(let item of source)
+			{
+				let obj=_.extend({}, item) ;
+				target.push(obj);
+				obj.id = item.key;
+				obj.name = item.value;
+			}
+		}
+
+        this._orderTypeDic = new DicLoader(restApiCfg, restApi, "ORDER", "TYPE");
+
+}
+	ngOnInit(){
+        this.layoutService.show();
+        
+        // this._buyerLoader.Go(null, [{key:"departmentId", value:null}])
+        // .then(success=>{
+        //    this._orderTypeDic.Go();
+        // })
+        // .catch(err=>{
+		// 	this.layoutService.hide();
+		// 	this.showMsg(err);
+		// });
+		this.layoutService.hide();
+	}
+
+
+
+showMsg(msg: string)
+	{
+		this._notice.open("COMMON.SYSTEM_PROMPT", msg);
+	}
+
+	
+}
