2c03a48202328cf285a12891a34f5a91419004f0
Chuyu Guo <chuyu.guo@hpe.com>
2017-02-22 17:46:54 +0800

    store-res  store-detail

--- backend/src/module/mtc-center/capacity-mng/service/store-detail.service.ts
+++ backend/src/module/mtc-center/capacity-mng/service/store-detail.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { RestApiCfg, RestApi, RestApiModel } from '../../../../architecture';
 
+import {StoreInfoList_mock} from '../model/store-info-list.mock';
 @Injectable()
 export class StoreDetailService {
     constructor(
@@ -9,4 +10,21 @@ export class StoreDetailService {
         private restApiCfg: RestApiCfg,
         private restApi: RestApi
     ) { }
+
+    init(): void {
+        this.restApiCfg.loadCfgData();
+    }
+
+    getStoreInfoList(StorageId:string): Promise<any> {
+        const pathParams = [
+            {
+                key: "storageId",
+                value: StorageId
+            }
+           
+        ];
+        //const api = this.restApiCfg.getRestApi("net-img.vm-mng.network.list");
+        //return this.restApi.request(api.method, api.url, pathParams, null, null);
+        return new Promise(resovle => setTimeout(resovle, 200)).then(() => StoreInfoList_mock);
+    }
 }
