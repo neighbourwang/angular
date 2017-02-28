d2570ce6023cc115f653b9be284a40be3d3c5a20
LilyWei <xiao-li.wei@hpe.com>
2017-02-28 14:21:33 +0800

    add number for unread msg

--- frontend/src/module/user-center/msg-mng/service/msg-mng.service.ts
+++ frontend/src/module/user-center/msg-mng/service/msg-mng.service.ts
@@ -29,9 +29,10 @@ export class MsgMngService {
                 value: status
             }
         ];
+        console.log("Service: status=", status, " pageIndex=", pageIndex, " pageSize=", pageSize);
         const api = this.restApiCfg.getRestApi("user-center.msg-mng.message.get");
-        //return this.restApi.request(api.method, api.url, pathParams, null, null);
-        return new Promise(resovle => setTimeout(resovle, 200)).then(() => { return MsgModel_mock });
+        return this.restApi.request(api.method, api.url, pathParams, null, null);
+        //return new Promise(resovle => setTimeout(resovle, 200)).then(() => { return MsgModel_mock });
     }
 
     setMsgRead(ids: string):Promise<any> {
