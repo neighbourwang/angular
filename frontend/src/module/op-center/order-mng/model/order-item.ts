2de31c568e4beba15afbb2ca447f20b0e0539e85
YXYFuture <1247676987@qq.com>
2017-03-17 18:56:51 +0800

    JIRA bug

--- frontend/src/module/op-center/order-mng/model/order-item.ts
+++ frontend/src/module/op-center/order-mng/model/order-item.ts
@@ -7,6 +7,7 @@ export class SubInstanceResp {
   orderNo: string = null;//, optional): 对应UI界面中的订单编号 ,
   purchaseDate: string = null;//, optional): 对应UI界面中的下单时间, 映射到后端的createDate
   canRenew:boolean = true;
+  canContinueRenew:boolean=true;//是否自动续订
   showInstance : boolean = true;//是否展示实例名称
   relySubinstanceId : number = null; //是否挂载了主机的标识
 
