5a92a97df0ced1291b86097544211111c7bb3a3f
YXYFuture <1247676987@qq.com>
2017-03-23 11:27:50 +0800

    JIRA

--- frontend/src/module/op-center/order-mng/component/vm-view.component.ts
+++ frontend/src/module/op-center/order-mng/component/vm-view.component.ts
@@ -6,10 +6,12 @@ import * as _ from 'underscore';
 	selector:'vm-view'
 	,template:`
 	<ul>
-		<li>{{ 'VM_INSTANCE.INSTANCE_NAME' | translate }}: {{_obj.instanceName}}</li>
-		<li>{{ 'COMMON.AVAILABLE_ZONE' | translate }}: {{_obj.zone}}</li>
-		<li>CPU: {{_obj.cpu}}</li>
-		<li>{{ 'COMMON.RAM' | translate }}: {{_obj.mem}}</li>
+		
+		<li>区域: {{_obj.instanceName}}</li>
+		<li>可用区: {{_obj.zone}}</li>
+		<li>实例规格: CPU:{{_obj.cpu}}/内存：{{_obj.mem}}/启动盘：{{_obj.bootstorage}}</li>
+		<li>密码: {{_obj.password}}</li>
+		<li>实例名称： {{_obj.instanceName}}</li>
 	</ul>
 	`
 })
@@ -17,19 +19,28 @@ export class VmViewComponent implements OnInit{
 	@Input()
 	private values:Array<SubInstanceAttrPair>;
 
-	private _obj:{instanceName:string;
-		zone:string;
-		cpu:string;
-		mem:string
+	private _obj:{
+		platform:string,//区域
+		zone:string;//可用区
+		cpu:string;//实例规格（cpu,内存,启动盘）
+		mem:string;
+		bootstorage:string;
+		//无ip地址和操作系统
+		password:string;//密码
+		instanceName:string;//实例名称
 	}
-
+// if(vm.attrCode== "PLATFORM"||vm.attrCode== "ZONE"||vm.attrCode== "CPU"||vm.attrCode== "MEM"||vm.attrCode== "BOOTSTORAGE"||vm.attrCode== "PASSWORD"||vm.attrCode== "INSTANCENAME")
+		
 	ngOnInit(){
 		let getProperty = _.property("attrDisplayValue");
 		this._obj = {
-			instanceName: getProperty(this.values.find(n=>n.attrCode == "INSTANCENAME"))
+			platform:getProperty(this.values.find(n=>n.attrCode == "PLATFORM"))
+			,zone:getProperty(this.values.find(n=>n.attrCode == 'ZONE'))
 			,cpu: getProperty(this.values.find(n=>n.attrCode == 'CPU'))
 			,mem: getProperty(this.values.find(n=>n.attrCode == 'MEM'))
-			,zone:getProperty(this.values.find(n=>n.attrCode == 'ZONE'))
+			,bootstorage:getProperty(this.values.find(n=>n.attrCode == 'BOOTSTORAGE'))
+			,password:getProperty(this.values.find(n=>n.attrCode == 'PASSWORD'))
+			,instanceName: getProperty(this.values.find(n=>n.attrCode == "INSTANCENAME"))
 		};
 
 		console.log('vm-view init', this.values);//.instanceName = 
