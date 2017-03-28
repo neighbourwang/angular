78a8995b40b905390342b39ba4e75770ea88857e
YXYFuture <1247676987@qq.com>
2017-03-28 11:36:13 +0800

    下载账单

--- frontend/src/module/op-center/order-mng/model/cost-pandect-item.ts
+++ frontend/src/module/op-center/order-mng/model/cost-pandect-item.ts
@@ -30,6 +30,9 @@ export class  TimeCaculater{
 
 //年份下拉列表
 	getYears(){
+        if(this.years.length>0){
+            this.years.splice(0,this.years.length);
+        }
 		for(let i = 1999; i<=this.currentYear ; i++){
 			let year = {id:i,name:i};
         	this.years.push(year);
@@ -118,6 +121,23 @@ export class  Chart{
         this.labels = [];
         this.options = null;
     }
-
-    
 }
+
+export class CostManageItem{
+
+     id:string;
+     
+     startTime:string;
+     endTime:string;//记账周期
+
+     money:string;//账单金额
+     endDate:string;//账单生成日
+     sentDate:string;//账单发送日
+     status:string;//账单状态
+     statusName:string;//用于显示
+
+    adjustAmount:string;//调整金额
+
+    adjustReason:string;//调整项目
+}
+
