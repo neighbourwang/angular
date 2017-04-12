5506ece749b5b4264009b45be1e76663180fbd87
huidan.fan <huidan.fan@hpe.com>
2017-04-12 16:31:11 +0800

    阿里云主账号本地mock基本完成

--- frontend/src/module/user-center/ali-cloud/component/ali-major-list.component.ts
+++ frontend/src/module/user-center/ali-cloud/component/ali-major-list.component.ts
@@ -5,6 +5,8 @@ import { Router } from '@angular/router';
 import { LayoutService, NoticeComponent , ConfirmComponent, PopupComponent, SystemDictionary, PaginationComponent  } from '../../../../architecture';
 
 //model
+import { AliMajorList } from '../model/ali-major-list.model';
+import { DepartList } from '../model/depart-list.model';
 
 //service
 import { AliMajorService } from '../service/ali-major-list.service';
@@ -38,25 +40,116 @@ export class AliMajorListComponent implements OnInit{
     noticeTitle = "";
     noticeMsg = "";
 
+    pageIndex= 1;
+    pageSize= 10;
+    totalPage= 1;
+
     type: string;
+    data: Array<AliMajorList>;
+    majorInfo: AliMajorList= new AliMajorList();
+    testInfo: boolean;
+    id: string;
+    departs: Array<DepartList>;
+
 
     ngOnInit (){
         console.log('init');
+        this.getData();
+    }
+
+    getData(pageIndex?) {
+        this.pageIndex= pageIndex || this.pageIndex;
+        this.layoutService.show();
+        this.service.getData(this.pageIndex, this.pageSize)
+            .then(
+                response => {
+                    this.layoutService.hide();
+                    if (response && 100 == response["resultCode"]) {
+                        this.data = response["resultContent"];
+                        this.totalPage= response.pageInfo.totalPage;
+                        console.log(this.data,"this.data");
+                    } else {
+                        this.showAlert("COMMON.OPERATION_ERROR");
+                    }
+                }
+            )
+            .catch((e) => this.onRejected(e));
     }
 
     editPage(item){
         this.type= "edit";
-        this.majorMng.open("编辑登录信息");
+        this.id= item.id;
+        this.layoutService.show();
+        this.service.updateInfo(this.id)
+            .then(
+                response => {
+                    this.layoutService.hide();
+                    if (response && 100 == response["resultCode"]) {
+                        this.majorInfo = response["resultContent"];
+                        this.majorMng.open("编辑登录信息");
+                        console.log("editInfo",this.majorInfo);
+                    } else {
+                        this.showAlert("COMMON.OPERATION_ERROR");
+                    }
+                }
+            )
+            .catch((e) => this.onRejected(e));
     }
 
     getDetail(item){
         this.type= "info";
-        this.majorMng.open("主账号详情");
+        this.id= item.id;
+        this.layoutService.show();
+        this.service.getDetail(this.id)
+            .then(
+                response => {
+                    this.layoutService.hide();
+                    if (response && 100 == response["resultCode"]) {
+                        this.majorInfo = response["resultContent"];
+                        this.majorMng.open("主账号详情");
+                        console.log("majorInfo",this.majorInfo);
+                    } else {
+                        this.showAlert("COMMON.OPERATION_ERROR");
+                    }
+                }
+            )
+            .catch((e) => this.onRejected(e));
+    }
+
+    testMajor(){
+        this.layoutService.show();
+        this.service.testMajor()
+            .then(
+                response => {
+                    this.layoutService.hide();
+                    if (response && 100 == response["resultCode"]) {
+                        this.testInfo= true;
+                    }else if(response.resultCode == 500){
+                        this.testInfo= false;
+                    }else {
+                        this.showAlert("COMMON.OPERATION_ERROR");
+                    }
+                }
+            )
+            .catch((e) => this.onRejected(e));
     }
 
     distriPage(item){
         this.type= "distribute";
-        this.distriDepart.open("分配部门")
+        this.layoutService.show();
+        this.service.departMajor()
+            .then(
+                response => {
+                    this.layoutService.hide();
+                    if (response && 100 == response["resultCode"]) {
+                        this.departs= response["resultContent"];
+                        this.distriDepart.open("分配部门");
+                    }else {
+                        this.showAlert("COMMON.OPERATION_ERROR");
+                    }
+                }
+            )
+            .catch((e) => this.onRejected(e));
     }
 
     gotoSubMng(){
@@ -67,7 +160,20 @@ export class AliMajorListComponent implements OnInit{
         if(this.type== "info"){
             this.majorMng.close();
         }else if(this.type== "edit"){
-
+            this.layoutService.show();
+            this.service.edit(this.id)
+                .then(
+                    response => {
+                        this.layoutService.hide();
+                        if (response && 100 == response["resultCode"]) {
+                            this.getData();
+                            this.majorMng.close();
+                        }else {
+                            this.showAlert("COMMON.OPERATION_ERROR");
+                        }
+                    }
+                )
+                .catch((e) => this.onRejected(e));
         }else{
 
         }
