87d086dda28b28ec972a598df10e96eb9e3b1962
neighbourwang <yao-yao.wang@hpe.com>
2017-04-13 12:36:33 +0800

    platform detail translate

---
+++ backend/src/module/prod-mng/physical-product-mng/component/physical-prod-cre-step1.component.ts
@@ -0,0 +1,58 @@
+import { Component, ViewChild, OnInit } from '@angular/core';
+
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+import { LayoutService, NoticeComponent, ConfirmComponent, PopupComponent } from '../../../../architecture';
+
+//model 
+
+// service;
+
+
+@Component({
+    templateUrl: '../template/physical-prod-cre-step1.html',
+    styleUrls: [],
+    providers: []
+})
+
+export class PhysicalProdCreStep1Component implements OnInit {
+
+
+    constructor(
+        private route: Router,
+        private router: ActivatedRoute,
+        private LayoutService: LayoutService,
+    ) { }
+
+
+    @ViewChild('notice')
+    notice: NoticeComponent;
+
+    @ViewChild('regionSelect')
+    regionSelect: PopupComponent;
+
+    prodDirType: string = "";
+    prodDirId: string = "";
+    ngOnInit() {
+        this.router.params.forEach((params: Params) => {
+            
+        })
+        if (this.prodDirId) {
+           
+        }
+    }
+
+    // 下一步
+    ccf() { }
+    //获取platformRegionList
+    // platFormRegionList:;
+
+    next() {
+        this.route.navigate(["prod-mng/prod-mng/prod-mng-cre-2"]);
+    }
+    //取消
+    cancel() {
+        this.route.navigateByUrl('prod-mng/prod-mng/prod-mng', { skipLocationChange: true })
+    }
+
+}
