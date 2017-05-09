f893cb39282236dee883461f34379d75bc936c46
qing-ren-zi <qing-ren.zi@hpe.com>
2017-05-09 16:39:27 +0800

    管理服务订购完善

--- frontend/src/module/cloud-host-service/management-services/model/other.model.ts
+++ frontend/src/module/cloud-host-service/management-services/model/other.model.ts
@@ -7,17 +7,19 @@ class Values {
 	REGION: ValuesAttr = new ValuesAttr;
 	ZONE: ValuesAttr = new ValuesAttr;
 	INSTANCETYPE: ValuesAttr = new ValuesAttr;
-	INSTANCEID: ValuesAttr = new ValuesAttr;
+	// INSTANCEID: ValuesAttr = new ValuesAttr;
 	INSTANCENAME: ValuesAttr = new ValuesAttr;
 	REMARK: ValuesAttr = new ValuesAttr;
 	SERVICEOBJECTCODE: ValuesAttr = new ValuesAttr;
 	TIMELINE: ValuesAttr = new ValuesAttr;
+	instanceId?: string;
 }
 
 class Selected {
 	REGION: ValuesAttr = new ValuesAttr;
 	ZONE: ValuesAttr = new ValuesAttr;
-	INSTANCEID: ValuesAttr = new ValuesAttr;
+	// INSTANCEID: ValuesAttr = new ValuesAttr;
+	instanceId?: string;
 	INSTANCENAME: ValuesAttr = new ValuesAttr;
 }
 
