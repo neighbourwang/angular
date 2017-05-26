d8c232e5e292b234cee9a42c50aa30e84fcd1357
neighbourwang <yao-yao.wang@hpe.com>
2017-05-26 11:26:32 +0800

    solve delete published product bug

--- backend/src/module/prod-mng/prod-mng/component/prod-mng.component.ts
+++ backend/src/module/prod-mng/prod-mng/component/prod-mng.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { LayoutService, NoticeComponent, ConfirmComponent, PopupComponent} from '../../../../architecture';
+import { LayoutService, NoticeComponent, ConfirmComponent, PopupComponent } from '../../../../architecture';
 //service
 import { ProdListService } from '../service/prodList.service';
 import { ProdDirListService } from '../service/prodDirList.service';
@@ -50,44 +50,44 @@ export class ProdMngComponent implements OnInit {
     notice: ConfirmComponent;
 
     @ViewChild('createProdPop')
-    createPop:PopupComponent;
+    createPop: PopupComponent;
 
     //平台
     platformsList = new Array();
-    platformId: string='';
+    platformId: string = '';
     //企业
     enterpriseList = new Array();
-    enterpriseId: string='';
+    enterpriseId: string = '';
     //产品目录列表
     prodDirList = new Array();
-    prodDirId: string='';
+    prodDirId: string = '';
     //创建用产品目录
-    prodDirIdCre: string='';
-    prodDirTypeCre: string='';
+    prodDirIdCre: string = '';
+    prodDirTypeCre: string = '';
     //初始化
-    ngOnInit() {        
+    ngOnInit() {
         this.backend(1, this.pp, {})
         this.getActivePlatform()
         this.getEnterpriseList()
         this.getServiceList();
-        this.ProdListService.productTypeDic.then(res=>console.log(res));
+        this.ProdListService.productTypeDic.then(res => console.log(res));
     }
     //获得激活云平台数据
-    getActivePlatform(){
+    getActivePlatform() {
         return this.PlatformsActiveService.getPlatformsActive().then(response => {
             console.log('激活云平台数据', response);
             // if (response && 100 == response.resultCode) {
-                this.platformsList = response.resultContent;
+            this.platformsList = response.resultContent;
             // } else {
 
             // }
         }).catch(err => {
             console.error(err)
         })
-    } 
+    }
     //获取企业列表    
-    getEnterpriseList(){
-       return  this.ProdDirListService.getEnterpriseList().then(response => {
+    getEnterpriseList() {
+        return this.ProdDirListService.getEnterpriseList().then(response => {
             console.log('企业', response);
             // if (response && 100 == response.resultCode) {
             this.enterpriseList = response.resultContent;
@@ -98,26 +98,26 @@ export class ProdMngComponent implements OnInit {
         }).catch(err => {
             console.error(err)
         })
-    } 
+    }
     //获取产品目录
-    getServiceList(){
+    getServiceList() {
         return this.ProdDirListService.getProdDirList().then(response => {
             console.log('产品目录列表', response);
             // if (response && 100 == response.resultCode) {
             this.prodDirList = response.resultContent;
-            if(response.resultContent[0]){
-                this.prodDirIdCre=response.resultContent[0].id;
-                this.prodDirTypeCre=response.resultContent[0].code;
-            }            
-            for(let i=0;i<this.prodDirList.length;i++){
-                if(!this.ProdDirListService[i]){
-                    this.prodDirList.slice(i,1);
+            if (response.resultContent[0]) {
+                this.prodDirIdCre = response.resultContent[0].id;
+                this.prodDirTypeCre = response.resultContent[0].code;
+            }
+            for (let i = 0; i < this.prodDirList.length; i++) {
+                if (!this.ProdDirListService[i]) {
+                    this.prodDirList.slice(i, 1);
                 }
             }
         }).catch(err => {
             console.error(err)
         })
-    }      
+    }
     // 选择产品目录（多选）
     switchSelectIndividual(id: number) {
         this.productList[id].isSelected =
@@ -131,10 +131,9 @@ export class ProdMngComponent implements OnInit {
             dir.isSelected = this.isSelectedAll;
         }
     }
-
     // 获得当前选中的产品目录
     getProduct() {
-        //radiio
+        //radio
         // let proddir : Proddir ;
         // for(let i = 0 ; i < this.prodDirList.length ; i ++){
         //     if(this.prodDirList[i].isSelected == true){
@@ -180,27 +179,26 @@ export class ProdMngComponent implements OnInit {
             message = message.substring(0, message.length - 1);
             switch (order) {
                 //PROD_MNG.DELETE_SELECTED_PRODUCT=>您选择删除{{value_1}}产品,请确认；如果确认，此产品目录的数据将不能恢复。
-                case 'delete': this.deleteConfirm.open('PROD_MNG.DELETE_PRODUCT', 'PROD_MNG.DELETE_SELECTED_PRODUCT^^^'+ message) //PROD_MNG.DELETE_PRODUCT=>删除产品 
-
+                case 'delete':
+                    if (this.prodList.find(v => v.status == 1)) {
+                        this.notice.open('COMMON.OPERATION_ERROR', '不能删除已发布的产品') //COMMON.OPERATION_ERROR=>操作错误  //PROD_MNG.NOT_ALLOW_TO_DELETE_PUBLISHED_PRODUCT_CAT=>不能删除状态为已发布的产品目录 
+                    } else {
+                        this.deleteConfirm.open('PROD_MNG.DELETE_PRODUCT', 'PROD_MNG.DELETE_SELECTED_PRODUCT^^^' + message) //PROD_MNG.DELETE_PRODUCT=>删除产品 
+                    }
                     break;
                 case 'publish':
                     if (this.prodList.find(v => v.status == 1)) {
                         this.notice.open('COMMON.OPERATION_ERROR', 'PROD_MNG.NOT_ALLOW_TO_PUBLISH_THE_PUBLISHED_PRODUCT'); //COMMON.OPERATION_ERROR=>操作错误  //PROD_MNG.NOT_ALLOW_TO_PUBLISH_THE_PUBLISHED_PRODUCT=>不可以再次发布已发布状态的产品 
-
-
                     } else {
-                        this.publishConfirm.open('PROD_MNG.PUBLISH_PRODUCT', 'PROD_MNG.PUBLISH_SELECTED_PRODUCT^^^'+message) ////PROD_MNG.PUBLISH_SELECTED_PRODUCT=>您选择发布{{value_1}}产品,请确认。
+                        this.publishConfirm.open('PROD_MNG.PUBLISH_PRODUCT', 'PROD_MNG.PUBLISH_SELECTED_PRODUCT^^^' + message) ////PROD_MNG.PUBLISH_SELECTED_PRODUCT=>您选择发布{{value_1}}产品,请确认。
 
                     }
                     break;
                 case 'ccPublish':
                     if (this.prodList.find(v => v.status == 3)) {
                         this.notice.open('COMMON.OPERATION_ERROR', 'PROD_MNG.DONOT_CANCEL_PUBLISH_FOR_PUBLISHED_PRODUCT'); //COMMON.OPERATION_ERROR=>操作错误  //PROD_MNG.DONOT_CANCEL_PUBLISH_FOR_PUBLISHED_PRODUCT=>不可以再次取消发布未发布状态的产品 
-
-
                     } else {
                         this.ccPublishConfirm.open('PROD_MNG.CANCEL_PUBLISH_PRODUCT', 'PROD_MNG.CANCEL_PUBLISH_PRODUCT_FOR_SELECTED^^^' + message); //PROD_MNG.CANCEL_PUBLISH_PRODUCT=>取消发布产品 
-
                     } break;
             }
         }
@@ -219,23 +217,23 @@ export class ProdMngComponent implements OnInit {
     publishCof() {
         console.log(this.prodList);
         this.ProdListService.changProdstatus({
-            "pids":this.changStatusIdList,
-            "status":"1"
-        }).then((response)=>{
-            this.query() ;
-        }).catch((err)=>{
+            "pids": this.changStatusIdList,
+            "status": "1"
+        }).then((response) => {
+            this.query();
+        }).catch((err) => {
             console.error(err)
-        })         
+        })
     }
     //取消发布
     ccPublishCof() {
         console.log(this.prodList);
         this.ProdListService.changProdstatus({
-            "pids":this.changStatusIdList,
-            "status":"3"
-        }).then((response)=>{
-            this.query() ;
-        }).catch((err)=>{
+            "pids": this.changStatusIdList,
+            "status": "3"
+        }).then((response) => {
+            this.query();
+        }).catch((err) => {
             console.error(err)
         })
     }
@@ -243,17 +241,17 @@ export class ProdMngComponent implements OnInit {
     //编辑按钮
     edit() {
         console.log('edit');
-    }    
+    }
     //去编辑详情页面
     goDetail(item) {
         console.log(item);
         // if(item.serviceType=='0'||item.serviceType=='1'||item.serviceType=='4'){
-        if(item.serviceType=='0'||item.serviceType=='1'){
-            this.router.navigate(["prod-mng/prod-mng/prod-detail", {id:item.id,type:item.serviceType}]);            
-        }else if(item.serviceType=='4'){
-            this.router.navigate(["prod-mng/physical-prod-mng/prod-mng-edit", {id:item.id,type:item.serviceType}]);                        
-        }else if(item.serviceType=='3'||item.serviceType=='5'){
-            this.router.navigate(["prod-mng/database-middleware-mng/database-middleware-product-edit", {id:item.id,serviceName:item.serviceName}]);                                    
+        if (item.serviceType == '0' || item.serviceType == '1') {
+            this.router.navigate(["prod-mng/prod-mng/prod-detail", { id: item.id, type: item.serviceType }]);
+        } else if (item.serviceType == '4') {
+            this.router.navigate(["prod-mng/physical-prod-mng/prod-mng-edit", { id: item.id, type: item.serviceType }]);
+        } else if (item.serviceType == '3' || item.serviceType == '5') {
+            this.router.navigate(["prod-mng/database-middleware-mng/database-middleware-product-edit", { id: item.id, serviceName: item.serviceName }]);
         }
     }
     backend(page: number, size: number, data: any) {
@@ -266,8 +264,8 @@ export class ProdMngComponent implements OnInit {
                 console.log(response);
                 if (response && 100 == response.resultCode) {
                     this.productList = response.resultContent;
-                    if(this.productList.length==0){
-                        this.notice.open('提示','未找到相关产品信息');
+                    if (this.productList.length == 0) {
+                        this.notice.open('提示', '未找到相关产品信息');
                     }
                     this.tp = response.pageInfo.totalPage;
                 }
@@ -292,30 +290,30 @@ export class ProdMngComponent implements OnInit {
         this.backend(page, this.pp, {});
     }
     //创建按钮    
-    createProd(){
+    createProd() {
         this.createPop.open('PROD_MNG.CREATE_PRODUCT')
     }
-    selectProDir(e){
+    selectProDir(e) {
         console.log(e);
-        this.prodDirList.filter(ele=>{
-            if(ele.id==e){
+        this.prodDirList.filter(ele => {
+            if (ele.id == e) {
                 console.log(ele);
-                this.prodDirTypeCre=ele.code;
+                this.prodDirTypeCre = ele.code;
                 return ele;
             }
         })
     }
-    otcreate(){
+    otcreate() {
         console.log(this.prodDirTypeCre);
-        if(this.prodDirTypeCre=='PHYMACHINE_SERVICE'){
-            this.router.navigate(["prod-mng/physical-prod-mng/prod-mng-cre-step1", {'id':this.prodDirIdCre,'type':this.prodDirTypeCre}]);            
-        }else if(this.prodDirTypeCre=='VITRUALDISK_SERVICE'||this.prodDirTypeCre=='VITRUALMACHINE_SERVICE'){
-            this.router.navigate(["prod-mng/prod-mng/prod-mng-cre-1", {'id':this.prodDirIdCre,'type':this.prodDirTypeCre}]);                       
-        }else if(this.prodDirTypeCre=='SUPERVISE_SERVICE'){
-            this.router.navigate(["prod-mng/manager-serve/manager-serve-product-cre-step1", {'id':this.prodDirIdCre,'type':this.prodDirTypeCre}]);            
-        }else if(this.prodDirTypeCre=='MiddleWare'||this.prodDirTypeCre=='Database'){
-            this.router.navigate(["prod-mng/database-middleware-mng/database-middleware-product-cre-step1",{'id':this.prodDirIdCre,'type':this.prodDirTypeCre}]);             
-        }        
+        if (this.prodDirTypeCre == 'PHYMACHINE_SERVICE') {
+            this.router.navigate(["prod-mng/physical-prod-mng/prod-mng-cre-step1", { 'id': this.prodDirIdCre, 'type': this.prodDirTypeCre }]);
+        } else if (this.prodDirTypeCre == 'VITRUALDISK_SERVICE' || this.prodDirTypeCre == 'VITRUALMACHINE_SERVICE') {
+            this.router.navigate(["prod-mng/prod-mng/prod-mng-cre-1", { 'id': this.prodDirIdCre, 'type': this.prodDirTypeCre }]);
+        } else if (this.prodDirTypeCre == 'SUPERVISE_SERVICE') {
+            this.router.navigate(["prod-mng/manager-serve/manager-serve-product-cre-step1", { 'id': this.prodDirIdCre, 'type': this.prodDirTypeCre }]);
+        } else if (this.prodDirTypeCre == 'MiddleWare' || this.prodDirTypeCre == 'Database') {
+            this.router.navigate(["prod-mng/database-middleware-mng/database-middleware-product-cre-step1", { 'id': this.prodDirIdCre, 'type': this.prodDirTypeCre }]);
+        }
     }
-       
+
 }
